1234f9b956ff6ed8fe067478c05b0d9d052a1a05
bhsd <2545473905@qq.com>
2022-04-25 22:31:50 -0700

    doc(token.js): document PHP parser behavior

--- token.js
+++ token.js
@@ -1,14 +1,32 @@
+/**
+ * PHP解析器的步骤：
+ * 1. 替换签名和{{subst:}}，参见Parser::preSaveTransform；这在revision中不可能保留，可以跳过
+ * 2. 移除特定字符\x7f和\x00，参见Parser::parse
+ * 3. 注释/扩展标签（'<'相关）和模板/模板变量/转换（'{'相关，注意rightmost法则，以及'|'是否属于内链），并标记有效换行，
+ *    参见Preprocessor_Hash::buildDomTreeArrayFromText
+ * 4. HTML标签（允许不匹配），参见Sanitizer::internalRemoveHtmlTags
+ * 5. 表格，参见Parser::handleTables
+ * 6. 水平线和状态开关，参见Parser::internalParse
+ * 7. 标题，参见Parser::handleHeadings
+ * 8. 内链，含文件和分类，参见Parser::handleInternalLinks2
+ * 9. 外链，参见Parser::handleExternalLinks
+ * 10. ISBN、RFC和自由外链，参见handleMagicLinks
+ * 11. 段落和列表，参见BlockLevelPass::execute
+ */
+
 class Token extends Array {
+	type;
+	parsed;
+
 	constructor(token = []) {
-		super(0);
-		if (Array.isArray(token)) {
-			this.push(...token);
-		} else if (typeof token === 'string') {
-			this.push(token);
+		if (typeof token === 'string') {
+			super(0);
+			this.push(token.replace(/[\x7f\x00]/g, '')); // eslint-disable-line no-control-regex
+			this.type = 'plain';
+			this.parsed = false;
 		} else {
-			throw new TypeError('仅接受Array或String作为输入参数！');
+			throw new TypeError('仅接受String作为输入参数！');
 		}
-		this.type = 'plain';
 	}
 
 	toString() {
