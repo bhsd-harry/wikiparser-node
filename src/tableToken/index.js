45e2ed04f30166d9d1dbf70e946d14f80ac4b1bb
bhsd <2545473905@qq.com>
2022-06-15 20:53:56 -0700

    chore(TableToken): temporarily remove some methods

--- src/tableToken/index.js
+++ src/tableToken/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const {externalUse, typeError} = require('../../util/debug'),
+const {externalUse} = require('../../util/debug'),
 	attributeParent = require('../../mixin/attributeParent'),
 	/** @type {Parser} */ Parser = require('../..'),
 	Token = require('../token');
@@ -20,7 +20,7 @@ class TableToken extends attributeParent(Token) {
 	 */
 	constructor(type, syntax, attr = '', config = Parser.getConfig(), accum = []) {
 		super(undefined, config, true, accum, {
-			String: 1, Token: 1, AttributeToken: 0, TableToken: '1:', TdToken: '1:',
+			String: 1, Token: 1, AttributeToken: 0, TableToken: type === 'table' ? '1:' : [], TdToken: '1:',
 		});
 		this.type = type;
 		this.#syntax = syntax;
@@ -71,6 +71,16 @@ class TableToken extends attributeParent(Token) {
 		return super.build();
 	}
 
+	insertAt(token, i) {
+		const TdToken = require('./tdToken');
+		if (this.type === 'table' && token instanceof TableToken && token.type === 'tr'
+			&& this.childNodes[i] instanceof TdToken
+		) {
+			throw new Error(`新的表格行不能插入到第 ${i} 个位置！`);
+		}
+		return super.insertAt(token, i);
+	}
+
 	toString(separator = '\n') {
 		if (this.type === 'table' && !this.#closing && this.nextSibling) {
 			Parser.error('自动闭合表格');
@@ -104,76 +114,6 @@ class TableToken extends attributeParent(Token) {
 				child.escape();
 			}
 		}
-	}
-
-	/**
-	 * @param {TdToken} td
-	 * @param {'afterbegin'|'beforeend'} position
-	 */
-	insertTableCell(td, position = 'beforeend') {
-		if (this.type === 'td') {
-			throw new Error('只能在表格或表格行插入新的单元格！');
-		} else if (!['afterbegin', 'beforeend'].includes(position)) {
-			throw new RangeError('插入位置只能选择"afterbegin"或"beforeend"！');
-		} else if (position === 'beforeend' && this.type === 'table') {
-			const {lastChild} = this;
-			if (lastChild instanceof TableToken && lastChild.type === 'tr') {
-				throw new Error('当前不能将单元格直接插入表格，请插入某一表格行。');
-			}
-		}
-		const TdToken = require('./tdToken');
-		if (!(td instanceof TdToken)) {
-			typeError('TdToken');
-		} else if (!td.toString().endsWith('\n')) {
-			const {lastElementChild} = td;
-			lastElementChild.appendChild('\n');
-			lastElementChild.normalize();
-		}
-		if (position === 'beforeend') {
-			return this.insertAt(td);
-		}
-		const [, child] = this.childNodes;
-		return this.insertAt(td, child !== undefined && !(child instanceof TableToken) ? 2 : 1);
-	}
-
-	/**
-	 * @param {'td'|'th'|'caption'}
-	 * @param {string} attr
-	 * @param {string|Token|(string|Token)[]} inner
-	 * @param {'afterbegin'|'beforeend'} position
-	 */
-	newTableCell(subtype, attr, inner, position = 'beforeend') {
-		if (!['td', 'th', 'caption'].includes(subtype)) {
-			throw new RangeError('单元格的子类型只能为"td"、"th"或"caption"！');
-		} else if (typeof attr !== 'string') {
-			typeError('String');
-		}
-		const TdToken = require('./tdToken'),
-			syntaxes = {td: '|', th: '!', caption: '|+'},
-			td = new TdToken(syntaxes[subtype], `${attr}${attr && '|'}`, this.getAttribute('config'));
-		td.lastElementChild.append(...Array.isArray(inner) ? inner : [inner]);
-		return this.insertTableCell(td, position);
-	}
-
-	/**
-	 * @param {string|TableToken} attr
-	 * @returns {TableToken}
-	 */
-	insertTableRow(attr, i = this.childNodes.length) {
-		if (typeof attr !== 'string' && !(attr instanceof TableToken && attr.type === 'tr')) {
-			typeError('String', 'TableToken');
-		} else if (typeof i !== 'number') {
-			typeError('Number');
-		} else if (this.type !== 'table') {
-			throw new Error('只能在表格内插入新行！');
-		}
-		const child = this.childNodes[i];
-		if (!(child instanceof TableToken) || child.type === 'td') {
-			throw new RangeError(`不能在第 ${i} 个位置处插入新行！`);
-		} else if (typeof attr === 'string') {
-			attr = new TableToken('tr', '|-', attr, this.getAttribute('config'));
-		}
-		return this.insertAt(attr, i);
 	}
 }
 
