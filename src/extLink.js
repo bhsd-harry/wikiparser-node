dc3385253a857fdd818cbff52aa8a94c1c8f6f5b
bhsd <2545473905@qq.com>
2022-07-04 09:41:48 -0700

    fix(ExtLinkToken): fix (continue)

--- src/extLink.js
+++ src/extLink.js
@@ -52,7 +52,7 @@ class ExtLinkToken extends Token {
 	#correct() {
 		if (!this.#space && this.childElementCount > 1
 			// 都替换成`<`肯定不对，但无妨
-			&& /^[^[\]<>"\x00-\x20\x7f\p{Zs}\ufffd]/u.test(this.lastElementChild.text().replace(/&[lg]t;/, '<'))
+			&& /^[^[\]<>"{\x00-\x20\x7f\p{Zs}\ufffd]/u.test(this.lastElementChild.text().replace(/&[lg]t;/, '<'))
 		) {
 			this.#space = ' ';
 		}
