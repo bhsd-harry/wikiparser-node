7b4740269f06b3619d31530d7a8b5f575195b240
bhsd <2545473905@qq.com>
2023-01-15 14:38:14 -0800

    style(node.d.ts): rearrange

--- typings/node.d.ts
+++ typings/node.d.ts
@@ -6,24 +6,24 @@ import ParameterToken from '../src/parameter';
 declare global {
 	type TokenAttribute<T> =
 		T extends 'stage' ? number :
-		T extends 'include'|'bracket' ? boolean :
+		T extends 'config' ? ParserConfig :
+		T extends 'accum' ? accum :
+		T extends 'parentNode' ? Token|undefined :
+		T extends 'childNodes' ? (AstText|Token)[] :
+		T extends 'parseOnce' ? (n: number, include: boolean) => Token :
+		T extends 'buildFromStr' ? (str: string) => (AstText|Token)[] :
+		T extends 'build' ? () => void :
+		T extends 'bracket'|'include' ? boolean :
 		T extends 'pattern' ? RegExp :
 		T extends 'optional'|'tags'|'flags' ? string[] :
 		T extends 'keys' ? Set<string> :
 		T extends 'attr' ? Map<string, string|true> :
 		T extends 'acceptable' ? Record<string, Ranges> :
 		T extends 'args' ? Record<string, Set<ParameterToken>> :
-		T extends 'config' ? ParserConfig :
-		T extends 'accum' ? accum :
 		T extends 'protectedChildren' ? Ranges :
-		T extends 'parentNode' ? Token|undefined :
-		T extends 'childNodes' ? (AstText|Token)[] :
 		T extends 'verifyChild' ? (i: number, addition: number) => void :
 		T extends 'matchesAttr' ? (key: string, equal: string, val: string, i: string) => boolean :
-		T extends 'parseOnce' ? (n: number, include: boolean) => Token :
-		T extends 'buildFromStr' ? (str: string) => (AstText|Token)[] :
 		T extends 'protectChildren' ? (...args: string|number|Range) => void :
-		T extends 'build' ? () => void :
 		string;
 
 	interface printOpt {
