6743f98a01156de43818e455ee89f9e0ccf17cf2
bhsd <2545473905@qq.com>
2023-01-15 14:05:47 -0800

    style(index.js): try to rearrange code

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const fs = require('fs'),
 	{text} = require('./util/string');
 
 const /** @type {Parser} */ Parser = {
-	warning: true,
-	debugging: false,
-	running: false,
-
 	config: './config/default',
 
 	MAX_STAGE: 11,
+
+	warning: true,
+	debugging: false,
+	running: false,
 
 	classes: {},
 	mixins: {},
@@ -33,6 +33,8 @@ const /** @type {Parser} */ Parser = {
 		['ConverterToken'],
 	],
 	typeAliases: {
+		text: ['string', 'str'],
+		plain: ['regular', 'normal'],
 		include: ['includeonly'],
 		ext: ['extension'],
 		'ext-attr': ['extension-attr'],
@@ -55,9 +57,12 @@ const /** @type {Parser} */ Parser = {
 		'td-inner': ['table-cell-inner', 'table-data-inner'],
 		'double-underscore': ['underscore', 'behavior-switch', 'behaviour-switch'],
 		hr: ['horizontal'],
+		link: ['wikilink'],
 		category: ['category-link', 'cat', 'cat-link'],
 		file: ['file-link', 'image', 'image-link', 'img', 'img-link'],
+		'link-text': ['wikilink-text'],
 		'gallery-image': ['gallery-file', 'gallery-img'],
+		'imagemap-image': ['imagemap-file', 'imagemap-img', 'image-map-image', 'image-map-file', 'image-map-img'],
 		'image-parameter': ['img-parameter', 'image-param', 'img-param'],
 		quote: ['quotes', 'quot', 'apostrophe', 'apostrophes', 'apos'],
 		'ext-link': ['external-link'],
@@ -74,71 +79,14 @@ const /** @type {Parser} */ Parser = {
 		'converter-rule-variant': ['convert-rule-variant', 'conversion-rule-variant'],
 		'converter-rule-to': ['convert-rule-to', 'conversion-rule-to'],
 		'converter-rule-from': ['convert-rule-from', 'conversion-rule-from'],
+		'imagemap-link': ['image-map-link'],
+		'param-line': ['parameter-line'],
 	},
 
 	promises: [Promise.resolve()],
 
-	warn(msg, ...args) {
-		if (this.warning) {
-			console.warn('\x1B[33m%s\x1B[0m', msg, ...args);
-		}
-	},
-	debug(msg, ...args) {
-		if (this.debugging) {
-			console.debug('\x1B[34m%s\x1B[0m', msg, ...args);
-		}
-	},
-	error(msg, ...args) {
-		console.error('\x1B[31m%s\x1B[0m', msg, ...args);
-	},
-	info(msg, ...args) {
-		console.info('\x1B[32m%s\x1B[0m', msg, ...args);
-	},
-
-	log(f) {
-		if (typeof f === 'function') {
-			console.log(String(f));
-		}
-	},
-
 	getConfig() {
 		return require(this.config);
-	},
-
-	run(callback) {
-		const {running} = this;
-		this.running = true;
-		try {
-			const result = callback();
-			this.running = running;
-			return result;
-		} catch (e) {
-			this.running = running;
-			throw e;
-		}
-	},
-
-	clearCache() {
-		const entries = [
-			...Object.entries(this.classes),
-			...Object.entries(this.mixins),
-			...Object.entries(this.parsers),
-			...Object.entries(this.tool),
-		];
-		for (const [, filePath] of entries) {
-			delete require.cache[require.resolve(filePath)];
-		}
-		for (const [name, filePath] of entries) {
-			if (name in global) {
-				global[name] = require(filePath);
-			}
-		}
-	},
-
-	isInterwiki(title, {interwiki} = Parser.getConfig()) {
-		title = String(title);
-		return new RegExp(`^(${interwiki.join('|')})\\s*:`, 'iu')
-			.exec(title.replaceAll('_', ' ').replace(/^\s*:?\s*/u, ''));
 	},
 
 	normalizeTitle(title, defaultNs = 0, include = false, config = Parser.getConfig(), halfParsed = false) {
@@ -225,6 +173,65 @@ const /** @type {Parser} */ Parser = {
 		return token;
 	},
 
+	warn(msg, ...args) {
+		if (this.warning) {
+			console.warn('\x1B[33m%s\x1B[0m', msg, ...args);
+		}
+	},
+	debug(msg, ...args) {
+		if (this.debugging) {
+			console.debug('\x1B[34m%s\x1B[0m', msg, ...args);
+		}
+	},
+	error(msg, ...args) {
+		console.error('\x1B[31m%s\x1B[0m', msg, ...args);
+	},
+	info(msg, ...args) {
+		console.info('\x1B[32m%s\x1B[0m', msg, ...args);
+	},
+
+	log(f) {
+		if (typeof f === 'function') {
+			console.log(String(f));
+		}
+	},
+
+	run(callback) {
+		const {running} = this;
+		this.running = true;
+		try {
+			const result = callback();
+			this.running = running;
+			return result;
+		} catch (e) {
+			this.running = running;
+			throw e;
+		}
+	},
+
+	clearCache() {
+		const entries = [
+			...Object.entries(this.classes),
+			...Object.entries(this.mixins),
+			...Object.entries(this.parsers),
+			...Object.entries(this.tool),
+		];
+		for (const [, filePath] of entries) {
+			delete require.cache[require.resolve(filePath)];
+		}
+		for (const [name, filePath] of entries) {
+			if (name in global) {
+				global[name] = require(filePath);
+			}
+		}
+	},
+
+	isInterwiki(title, {interwiki} = Parser.getConfig()) {
+		title = String(title);
+		return new RegExp(`^(${interwiki.join('|')})\\s*:`, 'iu')
+			.exec(title.replaceAll('_', ' ').replace(/^\s*:?\s*/u, ''));
+	},
+
 	reparse(date) {
 		const main = fs.readdirSync(path.join(__dirname, 'errors'))
 			.find(name => name.startsWith(date) && name.at(-1) === 'Z');
@@ -259,9 +266,9 @@ const /** @type {Parser} */ Parser = {
 
 const /** @type {PropertyDescriptorMap} */ def = {};
 for (const key in Parser) {
-	if (['aliases', 'MAX_STAGE', 'typeAliases', 'promises'].includes(key)) {
+	if (['MAX_STAGE', 'aliases', 'typeAliases', 'promises'].includes(key)) {
 		def[key] = {enumerable: false, writable: false};
-	} else if (!['config', 'isInterwiki', 'normalizeTitle', 'parse', 'getTool'].includes(key)) {
+	} else if (!['config', 'normalizeTitle', 'parse', 'isInterwiki', 'getTool'].includes(key)) {
 		def[key] = {enumerable: false};
 	}
 }
