5f9a99ecbdb89b671a7c38900b1a422da36c4932
bhsd <2545473905@qq.com>
2023-01-16 14:57:55 -0800

    chore: publish v0.6.2-b to npm

--- bump.sh
+++ bump.sh
@@ -1,11 +1,20 @@
 #!/usr/local/bin/bash
-webpack
-eslint .
-if [[ $? -eq 0 ]]
+if [[ $2 == 'npm' ]]
 then
+	sed -i '' -E "s/\"version\": \".+\"/\"version\": \"$1-b\"/" package.json
 	git add -A
-	git commit -m "chore: bump version to v$1-b"
+	git commit -m "chore: publish v$1-b to npm"
 	git push
-	git tag v$1-b
-	git push origin v$1-b
+	npm publish --tag browser
+else
+	webpack
+	eslint .	
+	if [[ $? -eq 0 ]]
+	then
+		git add -A
+		git commit -m "chore: bump version to v$1-b"
+		git push
+		git tag v$1-b
+		git push origin v$1-b
+	fi
 fi
