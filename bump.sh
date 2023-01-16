d6572fbb3638525352345ee09d5ceccf3212752a
bhsd <2545473905@qq.com>
2023-01-16 14:22:18 -0800

    feat(bump.sh): exit at error

--- bump.sh
+++ bump.sh
@@ -1,7 +1,11 @@
 #!/usr/local/bin/bash
 webpack
-git add -A
-git commit -m "chore: bump version to v$1-b"
-git push
-git tag v$1-b
-git push origin v$1-b
+eslint .
+if [[ $? -eq 0 ]]
+then
+	git add -A
+	git commit -m "chore: bump version to v$1-b"
+	git push
+	git tag v$1-b
+	git push origin v$1-b
+fi
