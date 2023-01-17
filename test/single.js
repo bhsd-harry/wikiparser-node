2e50af2090624d397517c52dafd0b2e6e2839296
bhsd <2545473905@qq.com>
2023-01-17 11:02:58 -0800

    feat(test/single.js): standardize

--- test/single.js
+++ test/single.js
@@ -1,9 +1,32 @@
 'use strict';
 
 const fs = require('fs'),
+	assert = require('assert'),
 	path = require('path'),
 	Parser = require('..');
 
 const wikitext = fs.readFileSync(path.join(__dirname, 'single-page.txt'), 'utf8');
 
-Parser.parse(wikitext).lint();
+console.time('parse');
+const token = Parser.parse(wikitext);
+console.timeEnd('parse');
+if (String(token) !== wikitext) {
+	throw new Error('解析过程中不可逆地修改了原始文本！');
+}
+
+console.time('print');
+const printed = token.print();
+console.timeEnd('print');
+const entities = {lt: '<', gt: '>', amp: '&'},
+	restored = printed.replaceAll(
+		/<[^<]+?>|&([lg]t|amp);/gu,
+		/** @param {string} s */ (_, s) => s ? entities[s] : '',
+	);
+if (restored !== wikitext) {
+	throw new Error('渲染HTML过程中不可逆地修改了原始文本！');
+}
+
+console.time('lint');
+const errors = token.lint();
+console.timeEnd('lint');
+assert.deepStrictEqual(errors, []);
