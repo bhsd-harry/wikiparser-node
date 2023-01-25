ad7e4f1ab47d3932199b6e5d65d95e7e820294aa
bhsd <2545473905@qq.com>
2023-01-25 11:31:25 -0800

    feat(test/single.js): checkout from browser

---
+++ test/single.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const fs = require('fs'),
+	assert = require('assert'),
+	path = require('path'),
+	Parser = require('..');
+Parser.config = require('../config/default');
+
+const wikitext = fs.readFileSync(path.join(__dirname, 'single-page.txt'), 'utf8');
+
+console.time('parse');
+const token = Parser.parse(wikitext);
+console.timeEnd('parse');
+if (String(token) !== wikitext) {
+	throw new Error('解析过程中不可逆地修改了原始文本！');
+}
+
+console.time('print');
+const printed = token.print();
+console.timeEnd('print');
+const entities = {lt: '<', gt: '>', amp: '&'},
+	restored = printed.replace(
+		/<[^<]+?>|&([lg]t|amp);/gu,
+		/** @param {string} s */ (_, s) => s ? entities[s] : '',
+	);
+if (restored !== wikitext) {
+	throw new Error('渲染HTML过程中不可逆地修改了原始文本！');
+}
+
+console.time('lint');
+const errors = token.lint();
+console.timeEnd('lint');
+assert.deepStrictEqual(errors, []);
