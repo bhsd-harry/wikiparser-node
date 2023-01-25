a9732b4434f8ed048d9d1509705f433a0e4f8bef
bhsd <2545473905@qq.com>
2023-01-25 11:48:28 -0800

    fix(test/single.js): no Token.print() available

--- test/single.js
+++ test/single.js
@@ -19,19 +19,6 @@ const wikitext = fs.readFileSync(path.join(__dirname, 'single-page.txt'), 'utf8'
 		throw new Error('解析过程中不可逆地修改了原始文本！');
 	}
 
-	console.time('print');
-	const printed = token.print();
-	console.timeEnd('print');
-	const entities = {lt: '<', gt: '>', amp: '&'},
-		restored = printed.replace(
-			/<[^<]+?>|&([lg]t|amp);/gu,
-			/** @param {string} s */ (_, s) => s ? entities[s] : '',
-		);
-	if (restored !== wikitext) {
-		await diff(wikitext, restored);
-		throw new Error('渲染HTML过程中不可逆地修改了原始文本！');
-	}
-
 	console.time('lint');
 	const errors = token.lint();
 	console.timeEnd('lint');
