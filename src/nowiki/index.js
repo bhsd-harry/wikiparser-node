743bdeb2dd8145928a00e0e5880558a04cab0649
bhsd <2545473905@qq.com>
2023-01-25 20:15:04 -0800

    fix(NowikiToken): no print() should be defined

--- src/nowiki/index.js
+++ src/nowiki/index.js
@@ -32,15 +32,6 @@ class NowikiToken extends fixedToken(Token) {
 			: super.lint(start);
 	}
 
-	/** @override */
-	print() {
-		const {type, name} = this;
-		return super.print({
-			class: type === 'ext-inner' && (name === 'templatestyles' || name === 'section') && String(this)
-				&& 'ext-inner wpb-error',
-		});
-	}
-
 	/**
 	 * @override
 	 * @this {NowikiToken & {firstChild: AstText, constructor: typeof NowikiToken}}
