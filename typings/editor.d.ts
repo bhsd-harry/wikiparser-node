d0daf28d801fcacf8ab3bce7adc8f7163b3b7030
bhsd <2545473905@qq.com>
2023-03-03 17:09:31 -0800

    feat(typings/editor.d.ts): update setI18N

--- typings/editor.d.ts
+++ typings/editor.d.ts
@@ -10,10 +10,12 @@ declare global {
 	}
 
 	interface wikiparse extends wikiparseFunc {
+		config: ParserConfig;
 		print: (wikitext: string, include: boolean, stage: number) => Promise<string[][]>;
 		lint: (wikitext: string, include: boolean) => Promise<LintError[]>;
 		setConfig: (config: ParserConfig) => void;
 		getConfig: () => Promise<ParserConfig>;
+		setI18N: (i18n: Record<string, string>) => void;
 		Printer: typeof Printer;
 		Linter: typeof Linter;
 	}
