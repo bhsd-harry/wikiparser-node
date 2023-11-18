e61397e62ecf4b480d1e634835d81e92cd2a4fb8
bhsd <2545473905@qq.com>
2023-11-18 15:54:55 +0800

    fix(index.ts): fix rootRequire for absolute path

--- index.ts
+++ index.ts
@@ -132,7 +132,9 @@ declare interface Parser {
  * @param file 文件名
  * @param dir 子路径
  */
-const rootRequire = (file: string, dir = ''): unknown => require(`../${file.includes('/') ? '' : dir}${file}`);
+const rootRequire = (file: string, dir = ''): unknown => require(
+	file.startsWith('/') ? file : `../${file.includes('/') ? '' : dir}${file}`,
+);
 
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 const Parser: Parser = {
