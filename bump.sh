09ebf19ba3d95e98296403b61abab92b96ab2e1d
bhsd <2545473905@qq.com>
2023-11-20 22:11:29 +0800

    chore(bump.sh): update

--- bump.sh
+++ bump.sh
@@ -4,15 +4,13 @@ then
 	sed -i '' -E "s/\"version\": \".+\"/\"version\": \"$1-b\"/" package.json
 	git add -A
 	git commit -m "chore: publish v$1-b to npm"
-	git push
 	npm publish --tag browser
 else
-	rm bundle/*.js
-	webpack
-	eslint . && stylelint extensions/*.css
+	npm run lint
 	if [[ $? -eq 0 ]]
 	then
-		sed -i '' -E "s|wikiparser-node@.+-b/bundle/|wikiparser-node@$1-b/bundle/|" extensions/base.js
+		sed -i '' -E "s|wikiparser-node@.+-b|wikiparser-node@$1-b|" extensions/base.ts
+		npm run build
 		git add -A
 		git commit -m "chore: bump version to v$1-b"
 		git push
