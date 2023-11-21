04fc1ec7964a0bff0733d5aac1af49a24790aabd
bhsd <2545473905@qq.com>
2023-11-21 21:46:39 +0800

    fix(test): test using wiki

--- bin/toc.ts
+++ bin/toc.ts
@@ -1,15 +1,18 @@
 import * as fs from 'fs';
+import * as path from 'path';
 
 const {argv: [,, filename]} = process;
 if (!filename) {
 	throw new RangeError('请指定文档文件！');
-} else if (!fs.existsSync(filename)) {
-	throw new RangeError(`文档 ${filename} 不存在！`);
+}
+const fullpath = path.join(__dirname, '..', '..', 'wiki', `${filename}.md`);
+if (!fs.existsSync(fullpath)) {
+	throw new RangeError(`文档 ${filename}.md 不存在！`);
 }
 
-const content = fs.readFileSync(filename, 'utf8');
+const content = fs.readFileSync(fullpath, 'utf8');
 if (/^- \[[^\]]+\]\(#[^)]+\)$/mu.test(content)) {
-	throw new Error(`文档 ${filename} 中已包含目录！`);
+	throw new Error(`文档 ${filename}.md 中已包含目录！`);
 }
 
 const toc = content.split('\n').filter(line => line.startsWith('#')).map(line => line.replace(
@@ -19,4 +22,4 @@ const toc = content.split('\n').filter(line => line.startsWith('#')).map(line =>
 	})`,
 )).join('\n');
 
-fs.writeFileSync(filename, `<details>\n\t<summary>目录</summary>\n\n${toc}\n</details>\n\n${content}`);
+fs.writeFileSync(fullpath, `<details>\n\t<summary>目录</summary>\n\n${toc}\n</details>\n\n${content}`);
--- test/test.ts
+++ test/test.ts
@@ -5,15 +5,18 @@ import Parser = require('../index');
 
 const {argv: [,, title = '']} = process;
 Parser.debugging = true;
+Object.assign(Parser, {assert});
 
-for (const file of fs.readdirSync(path.join(__dirname, '../wiki'))) {
+for (const file of fs.readdirSync(path.join(__dirname, '..', '..', 'wiki'))) {
 	if (file.endsWith('.md') && file.toLowerCase().includes(title.toLowerCase())) {
 		Parser.debug(file);
-		const md = fs.readFileSync(path.join(__dirname, '../wiki', file), 'utf8');
+		const md = fs.readFileSync(path.join(__dirname, '..', '..', 'wiki', file), 'utf8');
 		for (const [code] of md.matchAll(/(?<=```js\n).*?(?=\n```)/gsu)) {
 			try {
 				Parser.config = 'default';
 				delete Parser.i18n;
+				Parser.conversionTable.clear();
+				Parser.redirects.clear();
 				eval(code); // eslint-disable-line no-eval
 			} catch (e) {
 				Parser.error(code);
