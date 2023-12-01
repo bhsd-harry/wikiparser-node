bebcb0261936bf685482b7a627751190dc8f39ac
bhsd <2545473905@qq.com>
2023-12-01 15:27:37 +0800

    fix(test.js): fix condition

--- test/test.ts
+++ test/test.ts
@@ -9,7 +9,7 @@ const title = process.argv[2]?.toLowerCase();
 
 for (const file of fs.readdirSync(path.join(__dirname, '..', '..', 'wiki'))) {
 	const lcFile = file.toLowerCase();
-	if (!title || title.endsWith('.md') ? lcFile === title : file.endsWith('.md') && lcFile.includes(title)) {
+	if (file.endsWith('.md') && (!title || (title.endsWith('.md') ? lcFile === title : lcFile.includes(title)))) {
 		Parser.debug(file);
 		const md = fs.readFileSync(path.join(__dirname, '..', '..', 'wiki', file), 'utf8');
 		for (const [code] of md.matchAll(/(?<=```js\n).*?(?=\n```)/gsu)) {
