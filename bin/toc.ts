5e0f3095e1d816687588c88c4e8c7eb3b201efea
bhsd <2545473905@qq.com>
2023-12-11 21:44:11 +0800

    feat(toc.ts): update

--- bin/toc.ts
+++ bin/toc.ts
@@ -6,7 +6,7 @@ if (!filename) {
 	throw new RangeError('请指定文档文件！');
 }
 const fullpath = path.join(__dirname, '..', '..', 'wiki', `${filename}.md`),
-	isEnglish = filename.endsWith('.en');
+	isEnglish = filename.endsWith('-(EN)');
 if (!fs.existsSync(fullpath)) {
 	throw new RangeError(`文档 ${filename}.md 不存在！`);
 }
@@ -25,8 +25,8 @@ const toc = content.split('\n').filter(line => line.startsWith('#')).map(line =>
 
 fs.writeFileSync(fullpath, `<details>\n\t<summary>${
 	isEnglish ? 'Table of Contents' : '目录'
-}</summary>\n\n${toc}\n</details>\n\n# Other Languages\n\n- [${
+}</summary>\n\n${toc}\n\n</details>\n\n# Other Languages\n\n- [${
 	isEnglish ? '简体中文' : 'English'
 }](./${
-	isEnglish ? filename.slice(0, -3) : `${filename}.en`
+	isEnglish ? filename.slice(0, -5) : `${filename}-%28EN%29`
 })\n\n${content}`);
