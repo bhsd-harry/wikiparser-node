138bc20faa74ac0ceb7ba56c881cb5ffe9038c75
bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-01-04 20:35:05 +0800

    chore: bump version to v1.2.0-b

--- extensions/dist/base.js
+++ extensions/dist/base.js
@@ -1,6 +1,6 @@
 (() => {
 const workerJS = () => {
-    importScripts('https://testingcf.jsdelivr.net/gh/bhsd-harry/wikiparser-node@1.1.7-b/bundle/bundle.min.js');
+    importScripts('https://testingcf.jsdelivr.net/gh/bhsd-harry/wikiparser-node@1.2.0-b/bundle/bundle.min.js');
     const entities = { '&': 'amp', '<': 'lt', '>': 'gt' };
     self.onmessage = ({ data }) => {
         var _a;
