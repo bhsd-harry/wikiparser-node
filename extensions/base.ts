f0d5ac8f58bd3e7d5830682a012e5b4f540573fc
bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-01-04 10:13:03 +0800

    chore: bump version to v1.1.7-b

--- extensions/base.ts
+++ extensions/base.ts
@@ -5,7 +5,7 @@ declare type WorkerListener<T> = ({data: [rid, res, resRaw]}: {data: [number, T,
 
 /** web worker */
 const workerJS = (): void => {
-	importScripts('https://testingcf.jsdelivr.net/npm/wikiparser-node@1.1.6-b/bundle/bundle.min.js');
+	importScripts('https://testingcf.jsdelivr.net/npm/wikiparser-node@1.1.7-b/bundle/bundle.min.js');
 	const entities = {'&': 'amp', '<': 'lt', '>': 'gt'};
 
 	/** @implements */
