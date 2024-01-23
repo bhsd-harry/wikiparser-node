0c3c60d2afecd078a1acaaa8fb1d844b29a48788
bhsd <2545473905@qq.com>
2024-01-23 10:23:21 +0800

    feat(bin/declaration): clean declaration file

--- bin/declaration.ts
+++ bin/declaration.ts
@@ -1,15 +1,38 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+// eslint-disable-next-line @stylistic/max-len
+const regex = /declare [^\n]+?: \(+abstract new [^\n]+\n(.+)\}\) & typeof (\w+\.)?(\w+)\b.+(export [^\n]+ extends )\w+\b(.+?\n)/isu;
+
 for (const file of fs.readdirSync('dist/src/', {recursive: true}) as string[]) {
 	if (!file.endsWith('.d.ts')) {
 		continue;
 	}
 	const fullPath = path.join('dist/src', file),
-		content = fs.readFileSync(fullPath, 'utf8'),
-		mt = /^\}\) & typeof (?:parser\.)?(\w*token);$/imu.exec(content);
-	if (mt) {
+		content = fs.readFileSync(fullPath, 'utf8');
+	if (content.includes('(abstract new ')) {
 		// console.log(file);
-		fs.writeFileSync(fullPath, content.replace(/^(export declare class \w+token extends )\w+/imu, `$1${mt[1]}`));
+		fs.writeFileSync(
+			fullPath,
+			content.replace(
+				regex,
+				(_, mixins: string, parser: string | undefined, base: string, exp: string, impl: string): string => {
+					const str = `${exp}${base}${impl}${
+							mixins.replace(/\n\}\) & \{.+?(?=\n(?:\}|$))/gsu, '')
+								.split(/\n\}\) & \(+abstract new .+/u)
+								.map(mixin => mixin.slice(0, mixin.lastIndexOf('readonly'))
+									.replace(/^[ \t]+(?:$|readonly .+\n)/gmu, ''))
+								.join('\n')
+						}`,
+						regex2 = new RegExp(`import \\{\\s*${base}\\b.+?;\n`, 'su');
+					let imports = '';
+					if (parser && !regex2.test(content)) {
+						const original = fs.readFileSync(path.join('src', file.replace(/d\.ts$/u, 'ts')), 'utf8');
+						[imports] = regex2.exec(original)!;
+					}
+					return imports + str;
+				},
+			),
+		);
 	}
 }
