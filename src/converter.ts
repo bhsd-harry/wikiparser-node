767c16fbcddd6abf758f0766c4277061735506fe
bhsd <2545473905@qq.com>
2024-01-23 13:33:25 +0800

    Revert "Merge branch 'mini' into lint"
    This reverts commit e8d5ad3c058ed8097316dbf468e7a010abdcdbad, reversing
    changes made to 88dcdc7ff23404141e2c525557cc77297228c269.

--- src/converter.ts
+++ src/converter.ts
@@ -11,7 +11,7 @@ import {ConverterRuleToken} from './converterRule';
  * 转换
  * @classdesc `{childNodes: [ConverterFlagsToken, ...ConverterRuleToken]}`
  */
-export class ConverterToken extends flagsParent(Token) implements FlagsParentBase {
+export class ConverterToken extends flagsParent(Token) {
 	override readonly type = 'converter';
 
 	declare readonly childNodes: [ConverterFlagsToken, ...ConverterRuleToken[]];
