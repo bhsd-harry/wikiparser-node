61aafe58b7e67ea73087bceb0352d09d3e5425e4
bhsd <2545473905@qq.com>
2024-02-03 23:21:05 +0800

    fix(AttributesToken): inconsistent `print()`

--- src/attributes.ts
+++ src/attributes.ts
@@ -257,11 +257,7 @@ export abstract class AttributesToken extends Token {
 
 	/** @override */
 	override print(): string {
-		return String(this)
-			? `<span class="wpb-${this.type}">${this.childNodes.map(child => child.print(
-				child instanceof AtomToken && child.text().trim() ? {class: 'attr-dirty'} : undefined,
-			)).join('')}</span>`
-			: '';
+		return String(this) ? super.print() : '';
 	}
 
 	/* NOT FOR BROWSER */
