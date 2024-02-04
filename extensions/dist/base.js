0c929715023d2cbfacd2bb6c0d36691ff81d0c79
bhsd <2545473905@qq.com>
2024-02-04 14:56:06 +0800

    chore: publish v1.4.4-b to npm

--- extensions/dist/base.js
+++ extensions/dist/base.js
@@ -1,6 +1,6 @@
 (() => {
 const workerJS = () => {
-    importScripts('https://testingcf.jsdelivr.net/gh/bhsd-harry/wikiparser-node@1.4.4-b/bundle/bundle.min.js');
+    importScripts('https://testingcf.jsdelivr.net/npm/wikiparser-node@1.4.4-b/bundle/bundle.min.js');
     const entities = { '&': 'amp', '<': 'lt', '>': 'gt' };
     self.onmessage = ({ data }) => {
         const [command, qid, wikitext, include, stage] = data;
@@ -15,7 +15,7 @@ const workerJS = () => {
                 postMessage([qid, Parser.getConfig()]);
                 break;
             case 'json':
-                postMessage([qid, JSON.stringify(Parser.parse(wikitext, include).json())]);
+                postMessage([qid, Parser.parse(wikitext, include).json()]);
                 break;
             case 'lint':
                 postMessage([qid, Parser.parse(wikitext, include).lint(), wikitext]);
@@ -56,7 +56,7 @@ const getFeedback = (command, qid, strict, raw, ...args) => new Promise(resolve 
     worker.postMessage([command, qid, raw, ...args]);
 });
 const getConfig = () => getFeedback('getConfig', -3);
-const json = async (wikitext, include, qid) => JSON.parse(await getFeedback('json', qid, false, wikitext, include));
+const json = (wikitext, include, qid) => getFeedback('json', qid, false, wikitext, include);
 const print = (wikitext, include, stage, qid = -1) => getFeedback('print', qid, false, wikitext, include, stage);
 const lint = (wikitext, include, qid = -2) => getFeedback('lint', qid, true, wikitext, include);
 const wikiparse = { id: 0, setI18N, setConfig, getConfig, print, lint, json };
