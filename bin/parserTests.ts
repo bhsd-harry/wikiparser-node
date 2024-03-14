930d8c7b0af46e7a0a1b889379db5de2f7a0f199
bhsd <2545473905@qq.com>
2024-03-15 00:52:01 +0800

    style(AttributeToken): minimize branch diff

--- bin/parserTests.ts
+++ bin/parserTests.ts
@@ -9,9 +9,9 @@ const content = fs.readFileSync('test/parserTests.txt', 'utf8'),
 for (const [test] of content.matchAll(/^!!\s*test\n.+?^!!\s*end$/gmsu)) {
 	if (/^!!\s*html(?:\/php)?$/mu.test(test) && (!test.includes('options') || /^!!\s*options\n+!/mu.test(test))) {
 		try {
-			const [wikitext] = /(?<=^!!\s*wikitext\n).*?(?=\n?!!\s*[a-z/]+$)/msu.exec(test)!,
-				[html] = /(?<=^!!\s*html(?:\/php)?\n).*?(?=\n?!!\s*[a-z/]+$)/msu.exec(test)!,
-				[desc] = /(?<=^!!\s*test\n).*?(?=\n?!!\s*[a-z/]+$)/msu.exec(test)!;
+			const [wikitext] = /(?<=^!!\s*wikitext\n).*?(?=\n?!!\s*[a-z/+]+$)/msu.exec(test)!,
+				[html] = /(?<=^!!\s*html(?:\/php)?\n).*?(?=\n?!!\s*[a-z/+]+$)/msu.exec(test)!,
+				[desc] = /(?<=^!!\s*test\n).*?(?=\n?!!\s*[a-z/+]+$)/msu.exec(test)!;
 			tests.push({desc, wikitext, html, print: Parser.parse(wikitext).print()});
 		} catch {
 			console.error(test);
