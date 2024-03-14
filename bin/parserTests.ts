cf355f9ef9a9e0a2f31eaa2a4cdeeefca78d4ce7
bhsd <2545473905@qq.com>
2024-03-15 00:02:25 +0800

    feat(bin/parserTests): log description

--- bin/parserTests.ts
+++ bin/parserTests.ts
@@ -9,10 +9,13 @@ const content = fs.readFileSync('test/parserTests.txt', 'utf8'),
 for (const [test] of content.matchAll(/^!! test\n.+?^!! end$/gmsu)) {
 	if (/^!! html(?:\/php)?$/mu.test(test) && (!test.includes('options') || /^!! options\n+!/mu.test(test))) {
 		try {
-			const [wikitext] = /(?<=^!! wikitext\n).*?(?=\n!! [a-z/]+$)/msu.exec(test)!,
-				[html] = /(?<=^!! html(?:\/php)?\n).*?(?=\n!! [a-z/]+$)/msu.exec(test)!;
-			tests.push({wikitext, html, print: Parser.parse(wikitext).print()});
-		} catch {}
+			const [wikitext] = /(?<=^!! wikitext\n).*?(?=\n?!! [a-z/]+$)/msu.exec(test)!,
+				[html] = /(?<=^!! html(?:\/php)?\n).*?(?=\n?!! [a-z/]+$)/msu.exec(test)!,
+				[desc] = /(?<=^!! test\n).*?(?=\n?!! [a-z/]+$)/msu.exec(test)!;
+			tests.push({desc, wikitext, html, print: Parser.parse(wikitext).print()});
+		} catch {
+			console.error(test);
+		}
 	}
 }
 fs.writeFileSync('test/parserTests.json', JSON.stringify(tests, null, '\t'));
