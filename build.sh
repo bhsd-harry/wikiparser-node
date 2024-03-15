0e699704d5d72ec7ebe5c31dde6947a061102fb2
bhsd <2545473905@qq.com>
2024-03-16 00:48:46 +0800

    chore: bump version to v1.6.2

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
 rm -rf dist/
-tsc && npm run declaration
+tsc && npm run declaration && node ./dist/bin/parserTests.js
 if [[ $? -eq 0 ]]
 then
 	err=$(tsc --project tsconfig.dist.json 2>&1 | bash sed.sh -E '/error TS2309:/d')
