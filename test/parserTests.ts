36f9433361b88a2334104b614dd90a867fea4dce
bhsd <2545473905@qq.com>
2024-03-15 23:47:49 +0800

    feat(test/parserTests): throw if failed

--- test/parserTests.ts
+++ test/parserTests.ts
@@ -4,14 +4,20 @@ import Parser = require('../index');
 
 const tests: {wikitext: string, print: string}[] = require('wikiparser-node/test/parserTests.json');
 (async () => {
+	let failed = 0;
 	for (const [i, {wikitext, print}] of tests.entries()) {
 		try {
 			assert.equal(Parser.parse(wikitext).print(), print);
 		} catch (e) {
+			console.log(wikitext);
+			console.log();
 			if (e instanceof assert.AssertionError) {
-				console.log(wikitext);
 				await diff(e.actual as string, e.expected as string, i); // eslint-disable-line no-await-in-loop
 			}
+			failed++;
 		}
+	}
+	if (failed) {
+		throw new Error(`${failed} tests failed!`);
 	}
 })();
