a35575fc897667c2dddf9aaf119cf44b592a6e72
bhsd <2545473905@qq.com>
2024-03-16 17:00:43 +0800

    fix(test/parserTests): ignore test file names

--- test/parserTests.ts
+++ test/parserTests.ts
@@ -2,19 +2,21 @@ import * as assert from 'assert/strict';
 import {diff} from '../util/diff';
 import Parser = require('../index');
 
-const tests: {wikitext: string, print: string}[] = require('wikiparser-node/test/parserTests.json');
+const tests: {wikitext?: string, print?: string}[] = require('wikiparser-node/test/parserTests.json');
 (async () => {
 	let failed = 0;
 	for (const [i, {wikitext, print}] of tests.entries()) {
-		try {
-			assert.equal(Parser.parse(wikitext).print(), print);
-		} catch (e) {
-			console.log(wikitext);
-			console.log();
-			if (e instanceof assert.AssertionError) {
-				await diff(e.expected as string, e.actual as string, i); // eslint-disable-line no-await-in-loop
+		if (wikitext && print) {
+			try {
+				assert.equal(Parser.parse(wikitext).print(), print);
+			} catch (e) {
+				console.log(wikitext);
+				console.log();
+				if (e instanceof assert.AssertionError) {
+					await diff(e.expected as string, e.actual as string, i); // eslint-disable-line no-await-in-loop
+				}
+				failed++;
 			}
-			failed++;
 		}
 	}
 	if (failed) {
