1cdfd155ffdd51c22f1d588d5bf57c49addff24b
bhsd <2545473905@qq.com>
2024-05-20 12:11:56 +0800

    chore: publish v1.8.0-b to npm

--- extensions/dist/parserTests.js
+++ extensions/dist/parserTests.js
@@ -1,7 +1,7 @@
 (() => {
 "use strict";
 (async () => {
-    const tests = await (await fetch('./test/parserTests.json')).json(), select = document.querySelector('select'), pre = document.querySelector('pre'), container = document.getElementById('frame'), seen = new Set();
+    const tests = await (await fetch('./test/parserTests.json')).json(), select = document.querySelector('select'), pre = document.querySelector('pre'), container = document.getElementById('frame');
     Parser.config = await (await fetch('./config/default.json')).json();
     wikiparse.print = (wikitext, include, stage) => {
         const printed = Parser.parse(wikitext, include, stage).print();
@@ -25,17 +25,6 @@
             optgroup.append(option);
         }
     }
-    const findUnique = (html) => {
-        var _a, _b;
-        const temp = new Set();
-        for (const ele of (_a = html.match(/<\w.*?>/gu)) !== null && _a !== void 0 ? _a : []) {
-            const mt = /<(\w+(?=[\s/>]))(?:.*(\sclass="[^"]+"))?/u.exec(ele), tag = `<${mt[1]}${(_b = mt[2]) !== null && _b !== void 0 ? _b : ''}>`;
-            if (!seen.has(tag)) {
-                temp.add(tag);
-            }
-        }
-        return temp;
-    };
     select.addEventListener('change', () => {
         const { wikitext, html } = tests[Number(select.value)];
         pre.textContent = wikitext;
@@ -47,30 +36,9 @@
         }
         wikiparse.highlight(pre, false, true);
         select.selectedOptions[0].disabled = true;
-        const tags = findUnique(html);
-        console.info(tags);
-        for (const tag of tags) {
-            seen.add(tag);
-        }
     });
     container.addEventListener('click', e => {
         e.preventDefault();
     }, { capture: true });
-    document.body.addEventListener('keydown', e => {
-        if (e.metaKey && e.key === 'ArrowDown') {
-            e.preventDefault();
-            const { selectedIndex, options } = select;
-            for (let i = selectedIndex + 1; i < options.length; i++) {
-                if (!options[i].disabled) {
-                    const tags = findUnique(tests[i - 1].html);
-                    if (tags.size > 0) {
-                        select.selectedIndex = i;
-                        select.dispatchEvent(new Event('change'));
-                        break;
-                    }
-                }
-            }
-        }
-    });
 })();
 })();
--- extensions/parserTests.ts
+++ extensions/parserTests.ts
@@ -8,8 +8,7 @@ declare interface Test {
 	const tests: Test[] = await (await fetch('./test/parserTests.json')).json(),
 		select = document.querySelector('select')!,
 		pre = document.querySelector('pre')!,
-		container = document.getElementById('frame')!,
-		seen = new Set<string>();
+		container = document.getElementById('frame')!;
 	Parser.config = await (await fetch('./config/default.json')).json();
 	/** @implements */
 	wikiparse.print = (wikitext, include, stage): Promise<[number, string, string][]> => {
@@ -34,22 +33,6 @@ declare interface Test {
 			optgroup.append(option);
 		}
 	}
-
-	/**
-	 * Find unique tags in the HTML
-	 * @param html The HTML content
-	 */
-	const findUnique = (html: string): Set<string> => {
-		const temp = new Set<string>();
-		for (const ele of html.match(/<\w.*?>/gu) ?? []) {
-			const mt = /<(\w+(?=[\s/>]))(?:.*(\sclass="[^"]+"))?/u.exec(ele)!,
-				tag = `<${mt[1]}${mt[2] ?? ''}>`;
-			if (!seen.has(tag)) {
-				temp.add(tag);
-			}
-		}
-		return temp;
-	};
 	select.addEventListener('change', () => {
 		const {wikitext, html} = tests[Number(select.value)]!;
 		pre.textContent = wikitext!;
@@ -61,29 +44,8 @@ declare interface Test {
 		}
 		wikiparse.highlight!(pre, false, true);
 		select.selectedOptions[0]!.disabled = true;
-		const tags = findUnique(html);
-		console.info(tags);
-		for (const tag of tags) {
-			seen.add(tag);
-		}
 	});
 	container.addEventListener('click', e => {
 		e.preventDefault();
 	}, {capture: true});
-	document.body.addEventListener('keydown', e => {
-		if (e.metaKey && e.key === 'ArrowDown') {
-			e.preventDefault();
-			const {selectedIndex, options} = select;
-			for (let i = selectedIndex + 1; i < options.length; i++) {
-				if (!options[i]!.disabled) {
-					const tags = findUnique(tests[i - 1]!.html);
-					if (tags.size > 0) {
-						select.selectedIndex = i;
-						select.dispatchEvent(new Event('change'));
-						break;
-					}
-				}
-			}
-		}
-	});
 })();
