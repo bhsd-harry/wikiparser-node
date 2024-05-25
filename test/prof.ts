570aa9101b9f3f419346c59179dab3e509e29866
bhsd <2545473905@qq.com>
2024-05-25 17:26:13 +0800

    chore(test/prof): ineffective for bundle

--- test/prof.ts
+++
@@ -1,9 +0,0 @@
-import {readFileSync} from 'fs';
-import {single} from './single';
-import '../../bundle/bundle.min.js';
-import type {Parser as ParserBase} from '../base';
-
-declare const Parser: ParserBase;
-
-const content = readFileSync('test/page.wiki', 'utf8');
-void single(Parser, {content, ns: 0, pageid: 0, title: 'Prof'});
