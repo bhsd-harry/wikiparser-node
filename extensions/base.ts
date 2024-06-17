dd26b05644ac51b7c7b1ca32c803c1398aa847c2
bhsd <2545473905@qq.com>
2024-06-18 02:03:00 +0800

    chore: publish v1.9.3-b to npm

--- extensions/base.ts
+++ extensions/base.ts
@@ -4,7 +4,7 @@ declare type WorkerListener<T> = ({data: [rid, res, resRaw]}: {data: [number, T,
 
 /** web worker */
 const workerJS = (): void => {
-	importScripts('https://testingcf.jsdelivr.net/gh/bhsd-harry/wikiparser-node@1.9.3-b/bundle/bundle.min.js');
+	importScripts('https://testingcf.jsdelivr.net/npm/wikiparser-node@1.9.3-b/bundle/bundle.min.js');
 	const entities = {'&': 'amp', '<': 'lt', '>': 'gt'};
 
 	/** @implements */
