4d751f39d77629bbdfa3ec223f6232b9d2c659c9
bhsd <2545473905@qq.com>
2024-06-25 11:56:54 +0800

    Revert "style(mixin/hidden): minimize branch diff"
    This reverts commit dbe3224327d9a74854a47955b280f77a1b5c5696.

--- mixin/hidden.ts
+++ mixin/hidden.ts
@@ -9,35 +9,33 @@ import type {LintError} from '../base';
  * @param constructor 基类
  * @param _ context
  */
-export const hiddenToken = (
-	linter = true,
-	html = true,
-) => <T extends AstConstructor>(constructor: T, _?: unknown): T => {
-	/** 解析后不可见的类 */
-	abstract class AnyHiddenToken extends constructor {
-		/** 没有可见部分 */
-		override text(): string {
-			return '';
-		}
+export const hiddenToken = (linter = true, html = true) =>
+	<T extends AstConstructor>(constructor: T, _?: unknown): T => {
+		/** 解析后不可见的类 */
+		abstract class AnyHiddenToken extends constructor {
+			/** 没有可见部分 */
+			override text(): string {
+				return '';
+			}
 
-		override lint(start?: number): LintError[] {
-			// @ts-expect-error private argument
-			return linter ? [] : super.lint(start);
-		}
+			override lint(start?: number): LintError[] {
+				// @ts-expect-error private argument
+				return linter ? [] : super.lint(start);
+			}
 
-		/* NOT FOR BROWSER */
+			/* NOT FOR BROWSER */
 
-		/** @private */
-		dispatchEvent(): void { // eslint-disable-line @typescript-eslint/class-methods-use-this
-			//
-		}
+			/** @private */
+			dispatchEvent(): void { // eslint-disable-line @typescript-eslint/class-methods-use-this
+				//
+			}
 
-		override toHtml(): string {
-			return html ? '' : super.toHtml();
+			override toHtml(): string {
+				return html ? '' : super.toHtml();
+			}
 		}
-	}
-	mixin(AnyHiddenToken, constructor);
-	return AnyHiddenToken;
-};
+		mixin(AnyHiddenToken, constructor);
+		return AnyHiddenToken;
+	};
 
 mixins['hiddenToken'] = __filename;
