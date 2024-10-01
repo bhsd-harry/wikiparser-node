356938da2f0a1bfd3d3efd6024972c9d90403657
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-10-01 21:15:28 +0800

    revert(constants): move `stages` to base.ts

--- util/constants.ts
+++ util/constants.ts
@@ -7,32 +7,6 @@ export enum BuildMethod {
 	Text,
 }
 
-export const stages = {
-	redirect: 1,
-	comment: 1,
-	ext: 1,
-	include: 1,
-	noinclude: 1,
-	arg: 2,
-	template: 2,
-	'magic-word': 2,
-	heading: 2,
-	html: 3,
-	table: 4,
-	hr: 5,
-	'double-underscore': 5,
-	link: 6,
-	file: 6,
-	category: 6,
-	quote: 7,
-	'ext-link': 8,
-	'free-ext-link': 9,
-	'magic-link': 9,
-	list: 10,
-	dd: 10,
-	converter: 11,
-};
-
 /* NOT FOR BROWSER */
 
 export const classes: Record<string, string> = {},
