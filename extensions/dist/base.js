c8b55d158635cfece00ad692fd839d7d82bd89ac
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-11-01 18:30:14 +0800

    chore: publish v1.13.1-b to npm

--- extensions/dist/base.js
+++ extensions/dist/base.js
@@ -1,7 +1,7 @@
 (() => {
 const version = '1.13.1';
 const workerJS = () => {
-    importScripts(`https://testingcf.jsdelivr.net/gh/bhsd-harry/wikiparser-node@$VERSION-b/bundle/bundle.min.js`);
+    importScripts(`https://testingcf.jsdelivr.net/npm/wikiparser-node@$VERSION-b/bundle/bundle.min.js`);
     const entities = { '&': 'amp', '<': 'lt', '>': 'gt' };
     self.onmessage = ({ data }) => {
         const [command, qid, wikitext, include, stage] = data;
