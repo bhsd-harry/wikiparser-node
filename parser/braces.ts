0f23ec4d2935e45b16f89dc05fd237fef7660e63
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-11-04 16:29:16 +0800

    prof(parser/braces): simple templates

--- parser/braces.ts
+++ parser/braces.ts
@@ -1,4 +1,4 @@
-import {removeComment, escapeRegExp} from '../util/string';
+import {removeComment} from '../util/string';
 import {HeadingToken} from '../src/heading';
 import {TranscludeToken} from '../src/transclude';
 import {ArgToken} from '../src/arg';
@@ -19,8 +19,8 @@ const closes: Record<string, string> = {
 		'-': String.raw`\}-`,
 		'[': String.raw`\]\]`,
 	},
-	marks = new Map([['!', '!'], ['!!', '+'], ['(!', '{'], ['!)', '}'], ['!-', '-'], ['=', '~']]),
-	re = new RegExp(String.raw`\{\{\s*(${[...marks.keys()].map(escapeRegExp).join('|')})\s*\}\}(?!\})`, 'gu');
+	marks = new Map([['!', '!'], ['!!', '+'], ['(!', '{'], ['!)', '}'], ['!-', '-'], ['=', '~'], ['server', 'm']]),
+	re = /\{\{\s*([^\s\0<>[\]{}|_#&%:.]+)\s*\}\}(?!\})/gu;
 
 /**
  * 解析花括号
@@ -39,7 +39,7 @@ export const parseBraces = (wikitext: string, config: Config, accum: Token[]): s
 	wikitext = wikitext.replace(re, (m, p1: string) => {
 		// @ts-expect-error abstract class
 		new TranscludeToken(m.slice(2, -2), [], config, accum);
-		return `\0${accum.length - 2}${marks.get(p1)}\x7F`;
+		return `\0${accum.length - 2}${marks.get(p1.toLowerCase()) ?? 't'}\x7F`;
 	});
 	const lastBraces = wikitext.lastIndexOf('}}') - wikitext.length;
 	// eslint-disable-next-line @typescript-eslint/no-unused-expressions
@@ -119,9 +119,9 @@ export const parseBraces = (wikitext: string, config: Config, accum: Token[]): s
 						accum,
 					);
 					const name = removeComment(parts![0]![0]!).trim();
-					if (marks.has(name)) {
-						ch = marks.get(name)!; // 标记{{!}}等
-					} else if (/^(?:filepath|(?:full|canonical)urle?):.|^server$/iu.test(name)) {
+					if (marks.has(name.toLowerCase())) {
+						ch = marks.get(name.toLowerCase())!; // 标记{{!}}等
+					} else if (/^(?:filepath|(?:full|canonical)urle?):./iu.test(name)) {
 						ch = 'm';
 					} else if (/^#vardefine:./iu.test(name)) {
 						ch = 'n';
