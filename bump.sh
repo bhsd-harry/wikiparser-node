cc24faac66d6accc4d3c60cae825547b88992971
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-11-04 21:14:09 +0800

    chore(bump.sh): publish browser scripts to main

--- bump.sh
+++ bump.sh
@@ -1,11 +1,18 @@
 #!/usr/local/bin/bash
 if [[ $2 == 'npm' ]]
 then
-	gsed -i -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
-	npm i --package-lock-only
-	npm publish --tag ${3-latest}
-	git add -A
-	git commit -m "chore: publish v$1 to npm"
+	npm view wikiparser-node@$1-b
+	if [[ $? -eq 0 ]]
+	then
+		git checkout browser bundle/bundle.min.js extensions/dist/*.js extensions/*.css
+		gsed -i 's/$VERSION-b/$VERSION/' extensions/dist/base.js
+		gsed -i -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
+		npm i --package-lock-only
+		npm publish --tag ${3-latest}
+		rm bundle/bundle.min.js extensions/dist/*.js extensions/*.css
+		git add -A
+		git commit -m "chore: publish v$1 to npm"
+	fi
 else
 	npm run lint && npm run build && npm test && npm run test:parser && npm run test:real
 	if [[ $? -eq 0 ]]
