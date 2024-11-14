e88ec345fd6c1646343b98c4af53efbe5566f3d2
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-11-14 12:12:25 +0800

    fix(bump.sh): update package-lock.json for workflow

--- bump.sh
+++ bump.sh
@@ -5,8 +5,6 @@ then
 	then
 		git checkout browser bundle/bundle.min.js extensions/dist/*.js extensions/*.css
 		gsed -i 's|/gh/bhsd-harry/wikiparser-node@${version}-b|/npm/wikiparser-node@${version}|' extensions/dist/base.js
-		gsed -i -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
-		npm i --package-lock-only
 		npm publish --tag ${3-latest}
 		if [[ -z $3 ]]
 		then
@@ -23,6 +21,8 @@ else
 	npm run lint && npm run build && npm test && npm run test:parser && npm run test:real
 	if [[ $? -eq 0 ]]
 	then
+		gsed -i -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
+		npm i --package-lock-only
 		git add -A
 		git commit -m "chore: bump version to v$1"
 		git push
