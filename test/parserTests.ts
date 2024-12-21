b52518e1a1fb976ccaf445e2aa43407e609e68a0
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-12-22 01:24:56 +0800

    test: mocha

--- test/parserTests.ts
+++ test/parserTests.ts
@@ -1,8 +1,8 @@
 import * as assert from 'assert/strict';
-import {diff} from '../util/diff';
 import Parser = require('../index');
 
 declare interface Test {
+	desc: string;
 	wikitext?: string;
 	print?: string;
 	render?: string;
@@ -26,29 +26,25 @@ for (const [name, target] of Object.entries(redirects)) {
 /* NOT FOR BROWSER END */
 
 const tests: Test[] = require('../../test/parserTests.json');
-(async () => {
-	let failed = 0;
-	for (const [i, {wikitext, print, render}] of tests.entries()) {
+describe('Parser tests', () => {
+	for (const {desc, wikitext, print, render} of tests) {
 		if (wikitext && (print || render)) {
-			const root = Parser.parse(wikitext);
-			try {
-				if (print) {
-					assert.equal(root.print(), print);
+			it(desc, () => {
+				const root = Parser.parse(wikitext);
+				try {
+					if (print) {
+						assert.equal(root.print(), print);
+					}
+					if (render) {
+						assert.equal(root.toHtml(), render);
+					}
+				} catch (e) {
+					if (e instanceof assert.AssertionError) {
+						e.cause = {message: `\n${wikitext}`};
+					}
+					throw e;
 				}
-				if (render) {
-					assert.equal(root.toHtml(), render);
-				}
-			} catch (e) {
-				console.log(wikitext);
-				console.log();
-				if (e instanceof assert.AssertionError) {
-					await diff(e.expected as string, e.actual as string, i);
-				}
-				failed++;
-			}
+			});
 		}
 	}
-	if (failed) {
-		throw new Error(`${failed} tests failed!`);
-	}
-})();
+});
--- test/test.ts
+++ test/test.ts
@@ -1,56 +1,61 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as assert from 'assert';
-import {info, error} from '../util/diff';
 import Parser = require('../index');
 
 Parser.warning = false;
-const title = process.argv[2]?.toLowerCase();
-let enCodes: string[] | undefined;
+const allCodes = new Map<string, string[]>();
 
-for (const file of fs.readdirSync(path.join(__dirname, '..', '..', 'wiki'))) {
-	const lcFile = file.toLowerCase();
-	if (file.endsWith('.md') && (!title || (title.endsWith('.md') ? lcFile === title : lcFile.includes(title)))) {
-		const md = fs.readFileSync(path.join(__dirname, '..', '..', 'wiki', file), 'utf8'),
-			codes = [...md.matchAll(/(?<=```js\n).*?(?=\n```)/gsu)]
-				.map(([code]) => code.replace(/[ \n]\/\/ .*$/gmu, ''));
-		let logging = true;
-		for (const code of codes) {
-			const lines = code.split('\n') as [string, ...string[]],
-				[first] = lines;
-			if (
-				first.endsWith(' (browser)')
-			) {
-				continue;
-			} else if (logging) {
-				info(file);
-				logging = false;
-			}
-			try {
-				Parser.i18n = undefined;
-				Parser.conversionTable.clear();
-				Parser.redirects.clear();
-				eval(code); // eslint-disable-line no-eval
-				if (code.includes('Parser.config = ')) {
-					Parser.config = 'default';
+describe('API tests', () => {
+	for (const file of fs.readdirSync(path.join(__dirname, '..', '..', 'wiki'))) {
+		if (file.endsWith('.md')) {
+			const md = fs.readFileSync(path.join(__dirname, '..', '..', 'wiki', file), 'utf8'),
+				codes = [...md.matchAll(/(?<=```js\n).*?(?=\n```)/gsu)]
+					.map(([code]) => code.replace(/[ \n]\/\/ .*$/gmu, ''));
+			allCodes.set(file.slice(0, -3), codes);
+			describe(file, () => {
+				beforeEach(() => {
+					Parser.i18n = undefined;
+					Parser.conversionTable.clear();
+					Parser.redirects.clear();
+				});
+				for (const code of codes) {
+					const lines = code.split('\n') as [string, ...string[]],
+						[first] = lines;
+					if (
+						!first.endsWith(' (browser)')
+					) {
+						it(first.slice(3), () => {
+							try {
+								eval(code); // eslint-disable-line no-eval
+								if (code.includes('Parser.config = ')) {
+									Parser.config = 'default';
+								}
+							} catch (e) {
+								if (e instanceof assert.AssertionError) {
+									e.cause = {message: lines[Number(/<anonymous>:(\d+)/u.exec(e.stack!)![1]) - 1]};
+								}
+								throw e;
+							}
+						});
+					}
 				}
-			} catch (e) {
-				error(code);
-				if (e instanceof assert.AssertionError) {
-					e.cause = lines[Number(/<anonymous>:(\d+)/u.exec(e.stack!)![1]) - 1];
-				}
-				throw e;
-			}
-		}
-		if (!title) {
-			if (file.endsWith('-(EN).md')) {
-				enCodes = codes;
-			} else if (enCodes) {
-				for (const [i, code] of codes.entries()) {
-					assert.strictEqual(code, enCodes[i], `${file} is different from its English version`);
-				}
-				enCodes = undefined;
-			}
+			});
 		}
 	}
-}
+});
+
+describe('Documentation tests', () => {
+	for (const [file, enCodes] of allCodes) {
+		if (file.endsWith('-(EN)')) {
+			const zhFile = file.slice(0, -5);
+			describe(zhFile, () => {
+				for (const [i, code] of allCodes.get(zhFile)!.entries()) {
+					it(code.split('\n')[0]!.slice(3), () => {
+						assert.strictEqual(code, enCodes[i], `${zhFile} is different from its English version`);
+					});
+				}
+			});
+		}
+	}
+});
