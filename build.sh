cdd3571ad384850567174e860fdb9af4218589b4
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-02-04 14:55:19 +0800

    chore(base): mark as PURE

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/usr/local/bin/bash
+
+function esbuild {
+	npx esbuild $1 --charset=utf8 --target=es2023 --format=esm --outfile=dist/$2
+}
+
 rm -rf dist/
-tsc && npm run declaration && npx esbuild ./util/sharable.ts --charset=utf8 --target=es2023 --format=esm --outfile=dist/util/sharable.mjs
+tsc && npm run declaration && esbuild ./base.ts base.mjs && esbuild ./util/sharable.ts util/sharable.mjs
 if [[ $? -eq 0 ]]
 then
 	mv dist/util/sharable.d.ts .
 	rm dist/internal.js dist/[abptu]*/*.d.ts
 	cp sharable.d.ts dist/util/sharable.d.mts
 	mv sharable.d.ts dist/util/
+	cp dist/base.d.ts dist/base.d.mts
 	bash sed.sh -i '/export declare const /,$d' dist/mixin/*.d.ts
 	echo 'declare global { type Acceptable = unknown; }' >> dist/index.d.ts
 	bash sed.sh -i '/export = Parser/i \
