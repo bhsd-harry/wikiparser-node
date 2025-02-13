6200dd6970db8b94d24ca0aa3dab922380041c83
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-02-13 12:56:02 +0800

    fix(ListToken): syntax

--- src/nowiki/list.ts
+++ src/nowiki/list.ts
@@ -9,7 +9,7 @@ import {syntax} from '../../mixin/syntax';
 /* NOT FOR BROWSER END */
 
 /** 位于行首的`;:*#` */
-@sol(true) @syntax(/^[;:*#]+$/u)
+@sol(true) @syntax(/^[;:*#]+[^\S\n]*$/u)
 export abstract class ListToken extends ListBaseToken {
 	override get type(): 'list' {
 		return 'list';
