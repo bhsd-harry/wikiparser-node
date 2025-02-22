7f2147fdaae93d87b957faf4c7bcf9b054f1cc26
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-02-23 00:23:55 +0800

    chore(test/prof): minimize branch diff

--- test/prof.ts
+++ test/prof.ts
@@ -1,8 +1,19 @@
+import {
+	readFileSync,
+
+	/* NOT FOR BROWSER ONLY */
+
+	writeFileSync,
+} from 'fs';
+
+/* NOT FOR BROWSER ONLY */
+
 import * as inspector from 'inspector';
-import {readFileSync, writeFileSync} from 'fs';
 import single from './single';
 import lsp from './lsp';
 import Parser = require('../index');
+
+/* NOT FOR BROWSER ONLY END */
 
 const content = readFileSync('test/page.wiki', 'utf8'),
 	session = new inspector.Session(),
@@ -13,12 +24,21 @@ session.post('Profiler.enable', () => {
 		(async () => {
 			for (let i = 0; i < (Number(count) || 20); i++) {
 				const page: SimplePage = {content, ns: 0, pageid: 0, title: `Pass ${i}`};
+
+				/* NOT FOR BROWSER ONLY */
+
 				if (i === 0) {
 					void single(Parser, page);
 				}
 				await lsp(Parser, page);
+
+				/* NOT FOR BROWSER ONLY END */
+
 				console.log();
 			}
+
+			/* NOT FOR BROWSER ONLY */
+
 			session.post('Profiler.stop', (_, {profile: {nodes}}) => {
 				const useful = nodes.filter(({callFrame: {url}}) => url.startsWith('file:///'));
 				writeFileSync('test/prof.txt', JSON.stringify(useful, null, '\t'));
