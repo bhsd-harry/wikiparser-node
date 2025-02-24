fa36ed6b89335212e0714658cf4449083ea2a622
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-02-25 00:01:25 +0800

    feat(test/test): `json()`

--- test/single.ts
+++ test/single.ts
@@ -60,6 +60,12 @@ export default async ({pageid, title, ns, content}: SimplePage, method?: string)
 		}
 	}
 
+	if (!method || method === 'json') {
+		console.time(`json: ${title}`);
+		token.json();
+		console.timeEnd(`json: ${title}`);
+	}
+
 	if (!method || method === 'lint') {
 		console.time(`lint: ${title}`);
 		const errors = token.lint()
