2de4395fdd4ea7fe53e62027423d0f14b32a5bce
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-03-15 00:01:58 +0800

    feat(bin/config): adapt for any MediaWiki site

--- bin/config.js
+++ bin/config.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 'use strict';
 const /** @type {import('./config.ts').default} */ fetchConfig = require('../dist/bin/config.js').default;
-const [,, site, url, force, old] = process.argv;
-fetchConfig(site, url, force, old);
+const [,, site, url, force] = process.argv;
+fetchConfig(site, url, force);
