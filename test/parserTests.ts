aadfe95e123fa2de752669b79cc80b3cfdc2f6fe
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-03-18 13:40:34 +0800

    test(util/string): test `decodeHtmlBasic()`

--- test/parserTests.ts
+++ test/parserTests.ts
@@ -11,7 +11,7 @@ const tests: Test[] = require('../../test/parserTests.json');
 describe('Parser tests', () => {
 	for (const {desc, wikitext, print, render} of tests) {
 		if (
-			wikitext && (print || /* istanbul ignore next */ render)
+			wikitext && (print || render)
 		) {
 			it(desc, () => {
 				Parser.parse(wikitext);
