f8bee1839867bbf93f980c2b20cae7e24fc6af58
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-03-26 19:43:32 +0800

    style: avoid unnecessary `.toString(true)`

--- src/attribute.ts
+++ src/attribute.ts
@@ -121,7 +121,7 @@ export abstract class AttributeToken extends Token {
 		if (this.parentNode) {
 			this.#tag = this.parentNode.name;
 		}
-		this.setAttribute('name', this.firstChild.toString(true).trim().toLowerCase());
+		this.setAttribute('name', this.firstChild.text().trim().toLowerCase());
 		super.afterBuild();
 	}
 
