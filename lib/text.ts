68f3e3626bae33b7ff9673a6e42160262dc904dc
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-04-16 22:13:40 +0800

    fix(AstText): lower severity for disallowed tags

--- lib/text.ts
+++ lib/text.ts
@@ -37,7 +37,7 @@ const errorSyntaxUrl = new RegExp(source, 'giu'),
 		p: 'lonely-http',
 		i: 'lonely-http',
 	},
-	disallowedTags = [
+	disallowedTags = new Set([
 		'html',
 		'head',
 		'style',
@@ -63,7 +63,7 @@ const errorSyntaxUrl = new RegExp(source, 'giu'),
 		'option',
 		'select',
 		'textarea',
-	];
+	]);
 let wordRegex: RegExp;
 try {
 	// eslint-disable-next-line prefer-regex-literals, es-x/no-regexp-unicode-property-escapes
@@ -181,10 +181,11 @@ export class AstText extends AstNode {
 				endIndex = startIndex + length,
 				nextChar = rootStr[endIndex],
 				previousChar = rootStr[startIndex - 1],
-				severity = length > 1 && !/^(?:rfc|pmid|isbn)$/iu.test(error) && !(
-					char === '<' && !/[\s/>]/u.test(nextChar ?? '')
+				severity = length > 1 && !(
+					char === '<' && (/^<\s/u.test(error) || !/[\s/>]/u.test(nextChar ?? '') || disallowedTags.has(tag!))
 					|| isHtmlAttrVal && (char === '[' || char === ']')
 					|| magicLink && type === 'parameter-value'
+					|| /^(?:rfc|pmid|isbn)$/iu.test(error)
 				)
 				|| char === '{' && (nextChar === char || previousChar === '-')
 				|| char === '}' && (previousChar === char || nextChar === '-')
