beee6140be14f1aad93b0796eab7fc449e78d4df
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-05-15 11:23:39 +0800

    feat(diff.sh): `--minimal`

--- diff.sh
+++ diff.sh
@@ -1,9 +1,9 @@
 #!/usr/local/bin/bash
 if (( $# > 2 ))
 then
-	git diff --ignore-all-space --color-moved "$@"
+	git diff --ignore-all-space --color-moved --minimal "$@"
 else
-	git diff --ignore-all-space --color-moved "$@" -- *.ts \
+	git diff --ignore-all-space --color-moved --minimal "$@" -- *.ts \
 	bin/ \
 	config/ data/ i18n/ lib/ mixin/ parser/ src/ test/ util/
 fi
