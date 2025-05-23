a5e0b3c4ba69b7697efee2cc658ccfbe37c47cb1
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-05-23 15:46:32 +0800

    chore(package.json): target ES2017

--- .eslintrc.es8.cjs
+++ .eslintrc.es8.cjs
@@ -5,15 +5,11 @@ const config = require('@bhsd/common/eslintrc.dist.cjs');
 module.exports = {
 	...config,
 	extends: [
-		'plugin:es-x/restrict-to-es2016',
+		'plugin:es-x/restrict-to-es2017',
 	],
 	rules: {
 		'es-x/no-global-this': 0,
 		'es-x/no-malformed-template-literals': 0,
-		'es-x/no-object-entries': 0,
-		'es-x/no-object-getownpropertydescriptors': 0,
-		'es-x/no-object-values': 0,
 		'es-x/no-regexp-unicode-property-escapes': 0,
-		'es-x/no-string-prototype-padstart-padend': 0,
 	},
 };
