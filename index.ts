172911e0ecb594381c5885fb5e693a7c6f764d6a
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-06-26 17:48:15 +0800

    feat(LintConfiguration): `getSeverity()`

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ import type {
 } from './base';
 import type {Title, TitleOptions} from './lib/title';
 import type {LanguageService, QuickFixData} from './lib/lsp';
-import type {LintConfig} from './lib/lintConfig';
+import type {LintConfiguration} from './lib/lintConfig';
 import type {Token} from './internal';
 
 /* NOT FOR BROWSER ONLY */
@@ -36,7 +36,7 @@ declare interface Parser extends ParserBase {
 	/** @since v1.5.1 */
 	rules: readonly LintError.Rule[];
 	/** @since v1.22.0 */
-	lintConfig: LintConfig;
+	lintConfig: LintConfiguration;
 
 	/* NOT FOR BROWSER ONLY */
 
