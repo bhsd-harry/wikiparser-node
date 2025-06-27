790d0da00d1e9b8c45a67c04ea8a3016547eac51
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-06-27 14:03:58 +0800

    feat(ImagemapToken): custom severity

--- lib/lintConfig.ts
+++ lib/lintConfig.ts
@@ -30,6 +30,13 @@ const defaultLintConfig: LintConfig = {
 			// parameter: 2,
 		},
 	],
+	'invalid-imagemap': [
+		2,
+		{
+			// image: 2,
+			// link: 2,
+		},
+	],
 	'nested-link': [
 		2,
 		{
