9e46c4f765a03d26d10b9ed20be238e256d80776
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-07-25 19:47:41 +0800

    chore: bump version to v1.22.1-b

--- extensions/base.ts
+++ extensions/base.ts
@@ -13,7 +13,7 @@ import type {
 declare type WorkerListener<T> = (e: {data: [string, number, T, string]}) => void;
 declare type Token = ReturnType<typeof Parser['parse']>;
 
-const version = '1.22.0',
+const version = '1.22.1',
 	src = (document.currentScript as HTMLScriptElement | null)?.src,
 	file = /\/extensions\/dist\/base\.(?:min\.)?js$/u,
 	CDN = src && file.test(src)
