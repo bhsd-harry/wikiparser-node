525734ae42ae141eec93a27f0522bc9a044f989b
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-08-01 13:20:49 +0800

    feat(bump.sh): simplify

--- bump.sh
+++ bump.sh
@@ -1,14 +1,10 @@
 #!/usr/local/bin/bash
-if [[ $2 == 'npm' ]]
+npm run build && npm run lint && npm test && npm run test:real
+if [[ $? -eq 0 ]]
 then
+	gsed -i -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
+	git add -A
+	git commit -m "chore: bump version to v$1"
+	git push
 	npm publish --tag "${3-latest}"
-else
-	npm run build && npm run lint && npm test && npm run test:real
-	if [[ $? -eq 0 ]]
-	then
-		gsed -i -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
-		git add -A
-		git commit -m "chore: bump version to v$1"
-		git push
-	fi
 fi
