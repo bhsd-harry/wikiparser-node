cfcca947c8f506d4ccdc2c8932f08a244d4795b3
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-08-11 12:15:48 +0800

    feat(LinkToken): quick fix for nested-link

--- src/link/index.ts
+++ src/link/index.ts
@@ -1,3 +1,4 @@
+import {rawurldecode} from '@bhsd/common';
 import {generateForSelf} from '../../util/lint';
 import Parser from '../../index';
 import {LinkBaseToken} from './base';
@@ -19,13 +20,22 @@ export abstract class LinkToken extends LinkBaseToken {
 		return 'link';
 	}
 
+	/** link text / 链接显示文字 */
+	get innerText(): string {
+		return this.length > 1
+			? this.lastChild.text()
+			: rawurldecode(this.firstChild.text().replace(/^\s*:?/u, ''));
+	}
+
 	/** @private */
 	override lint(start = this.getAbsoluteIndex(), re?: RegExp): LintError[] {
 		const errors = super.lint(start, re),
 			rule = 'nested-link',
 			s = Parser.lintConfig.getSeverity(rule);
 		if (s && this.closest('ext-link-text')) {
-			errors.push(generateForSelf(this, {start}, rule, 'internal link in an external link', s));
+			const e = generateForSelf(this, {start}, rule, 'internal link in an external link', s);
+			e.suggestions = [{desc: 'delink', range: [start, e.endIndex], text: this.innerText}];
+			errors.push(e);
 		}
 		return errors;
 	}
