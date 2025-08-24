0892c1b362bbb91b5c8bc5422317cb4f20cba985
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-08-24 10:26:41 +0800

    feat(Parser): `configPaths`

--- index.ts
+++ index.ts
@@ -46,6 +46,8 @@ declare interface Parser extends ParserBase {
 
 	/* NOT FOR BROWSER ONLY */
 
+	configPaths: string[];
+
 	/** @private */
 	lintCSS: boolean;
 
@@ -181,6 +183,8 @@ const Parser = { // eslint-disable-line @typescript-eslint/no-redeclare
 
 	/* NOT FOR BROWSER ONLY */
 
+	configPaths: [],
+
 	lintCSS: true,
 
 	/* NOT FOR BROWSER ONLY END */
@@ -190,7 +194,17 @@ const Parser = { // eslint-disable-line @typescript-eslint/no-redeclare
 		/* NOT FOR BROWSER ONLY */
 
 		if (!config && typeof this.config === 'string') {
-			this.config = rootRequire(this.config, 'config') as ConfigData;
+			if (!path.isAbsolute(this.config)) {
+				for (const p of this.configPaths) {
+					try {
+						this.config = require(path.resolve(process.cwd(), p, this.config as string));
+						break;
+					} catch {}
+				}
+			}
+			if (typeof this.config === 'string') {
+				this.config = rootRequire(this.config, 'config') as ConfigData;
+			}
 			/* istanbul ignore if */
 			if (
 				this.config.doubleUnderscore.length < 3
@@ -397,7 +411,6 @@ const def: PropertyDescriptorMap = {
 		/* NOT FOR BROWSER ONLY */
 
 		'fetchConfig',
-		'lintCSS',
 	]);
 for (const key in Parser) {
 	if (!enumerable.has(key)) {
