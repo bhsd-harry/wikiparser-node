979f8a7d1b611f0e41ff9f578a3afbd7e4fdc23f
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-10-17 22:33:42 +0800

    fix(parseCommentAndExt): Safari compatibility

--- bump.sh
+++ bump.sh
@@ -8,26 +8,21 @@ if [[ $2 == 'npm' ]]
 then
 	if [[ $(git tag -l "v$1-b") ]]
 	then
-		git checkout browser bundle/bundle{,-lsp}.min.js extensions/dist/*.js extensions/*.css
-		npm publish --tag "${3-latest}"
-		if [[ -z $3 ]]
-		then
-			npm dist-tag add "wikiparser-node@$1" browser
-		fi
+		git checkout temp-browser bundle/bundle{,-lsp}.min.js extensions/dist/*.js extensions/*.css
+		npm publish --tag beta
 		rm bundle/bundle{,-lsp}.min.js extensions/dist/*.js extensions/*.css
 	else
 		echo "Tag v$1-b not found"
 		exit 1
 	fi
 else
-	npm run build && npm run lint && npm test && npm run test:real
+	npm run build && npm run lint && npm run test:parser && npm run test:real
 	if [[ $? -eq 0 ]]
 	then
 		gsed -i -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
 		npm i --package-lock-only
 		git add -A
 		git commit -m "chore: bump version to v$1"
-		git push
 		git tag "v$1"
 		git push origin "v$1"
 	fi
