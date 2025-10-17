f015bf0a31015b1e1fcca3b723ab37107ff56179
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-10-17 12:22:31 +0800

    fix(Token): optional vscode-css-languageservice

--- src/index.ts
+++ src/index.ts
@@ -736,7 +736,7 @@ export class Token extends AstElement {
 				}
 
 				/* NOT FOR BROWSER ONLY */
-			} else if (isAttr(this, true)) {
+			} else if (cssLSP && isAttr(this, true)) {
 				const rule = 'invalid-css',
 					s = lintConfig.getSeverity(rule),
 					sWarn = lintConfig.getSeverity(rule, 'warn');
@@ -744,7 +744,7 @@ export class Token extends AstElement {
 					const root = this.getRootNode(),
 						textDoc = new EmbeddedCSSDocument(root, this);
 					errors.push(
-						...cssLSP!.doValidation(textDoc, textDoc.styleSheet)
+						...cssLSP.doValidation(textDoc, textDoc.styleSheet)
 							.filter(
 								({code, severity}) => code !== 'css-ruleorselectorexpected' && code !== 'emptyRules'
 									&& (severity === 1 ? s : sWarn),
