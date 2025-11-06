435d01b3bba4ef56b5be45327d46ed4f92224f79
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-11-06 11:01:28 +0800

    feat(ListToken): lint redirect-like syntax

--- src/nowiki/list.ts
+++ src/nowiki/list.ts
@@ -1,4 +1,9 @@
+import {generateForSelf} from '../../util/lint';
+import Parser from '../../index';
 import {ListBaseToken} from './listBase';
+import type {LintError, TokenTypes} from '../../base';
+
+const linkTypes = new Set<TokenTypes | 'text' | undefined>(['link', 'category', 'file']);
 
 /**
  * `;:*#` at the start of a line
@@ -8,5 +13,35 @@ import {ListBaseToken} from './listBase';
 export abstract class ListToken extends ListBaseToken {
 	override get type(): 'list' {
 		return 'list';
+	}
+
+	override lint(start = this.getAbsoluteIndex()): LintError[] {
+		LINT: {
+			const rule = 'syntax-like',
+				s = Parser.lintConfig.getSeverity(rule, 'redirect'),
+				{innerText} = this;
+			if (s && innerText === '#') {
+				let {nextSibling} = this;
+				if (nextSibling?.type === 'list-range') {
+					nextSibling = nextSibling.firstChild;
+				}
+				if (nextSibling?.type === 'text' && linkTypes.has(nextSibling.nextSibling?.type)) {
+					/^redirect\s*(?::\s*)?$/iu; // eslint-disable-line @typescript-eslint/no-unused-expressions
+					const re = new RegExp(
+						String.raw`^(?:${this.getAttribute('config').redirection.join('|')})\s*(?::\s*)?$`,
+						'iu',
+					);
+					if (re.test(`#${nextSibling.data}`)) {
+						const e = generateForSelf(nextSibling, {start: start + 1}, rule, 'redirect-like', s);
+						e.startIndex--;
+						e.startCol--;
+						e.endIndex += 2;
+						e.endCol += 2;
+						return [e];
+					}
+				}
+			}
+			return [];
+		}
 	}
 }
