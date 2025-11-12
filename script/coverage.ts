9a4d4913bd1563c6d5967a6a232845bf3d1cdc4f
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-11-12 14:28:51 +0800

    fix(FileToken): render inline image

--- script/coverage.ts
+++ script/coverage.ts
@@ -17,8 +17,9 @@ declare interface Coverage {
 
 // eslint-disable-next-line n/no-missing-require
 const {total: {statements: {pct}}}: Coverage = require('../../coverage/coverage-summary.json');
-const colors = ['#4c1', '#dfb317', '#e05d44'] as const;
-const re = new RegExp(colors.join('|'), 'u');
+const colors = ['#4c1', '#dfb317', '#e05d44'] as const,
+	value = String(Math.round(pct));
+const re = new RegExp(String.raw`${colors.join('|')}|\b\d{2}(?=%)`, 'gu');
 let color: string;
 if (pct >= 80) {
 	[color] = colors;
@@ -28,6 +29,5 @@ if (pct >= 80) {
 	[,, color] = colors;
 }
 const svg = fs.readFileSync('coverage/badge.svg', 'utf8')
-	.replace(/\b\d{2}(?=%)/gu, String(Math.round(pct)))
-	.replace(re, color);
+	.replace(re, m => m.startsWith('#') ? color : value);
 fs.writeFileSync('coverage/badge.svg', svg);
