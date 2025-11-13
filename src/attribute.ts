128ccfad044c25798d560a27692ba22b93004649
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-11-13 22:40:28 +0800

    Revert "feat(ExtToken): convert `<syntaxhighlight>` to HTML"
    This reverts commit 1f084b21ca8de7559733f60a88b506fe1b4053f5.

--- src/attribute.ts
+++ src/attribute.ts
@@ -54,7 +54,7 @@ const insecureStyle =
  */
 export abstract class AttributeToken extends Token {
 	declare readonly name: string;
-	readonly #type;
+	#type;
 	#tag;
 	#equal;
 	#quotes: [string?, string?];
