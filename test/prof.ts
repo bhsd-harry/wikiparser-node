10f3d1b4c257c79931628beb8731eb06ad1c55c2
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-11-13 12:42:17 +0800

    refactor(test/prof): util from @bhsd/nodejs

--- test/prof.ts
+++ test/prof.ts
@@ -1,55 +1,29 @@
-import {
-	readFileSync,
-
-	/* NOT FOR BROWSER ONLY */
-
-	writeFileSync,
-} from 'fs';
+import {readFileSync} from 'fs';
 import single from './single';
 import type {SimplePage} from '@bhsd/test-util';
 
 /* NOT FOR BROWSER ONLY */
 
-import inspector from 'inspector';
 import {register} from 'module';
 import {pathToFileURL} from 'url';
 import path from 'path';
+import {profile} from '@bhsd/nodejs';
 import lsp from './lsp';
-import type {Profiler} from 'inspector';
-
-declare interface ProfileNode extends Pick<Profiler.ProfileNode, 'callFrame' | 'hitCount'> {
-	positionTicks: Record<number, number>;
-}
 
 register(pathToFileURL(path.join(__dirname, 'hooks.js')));
-
-/**
- * Adds the ticks to the myTicks object.
- * @param myTicks
- * @param positionTicks
- */
-const addTicks = (myTicks: Record<number, number>, positionTicks?: Profiler.PositionTickInfo[]): void => {
-	if (positionTicks) {
-		for (const {line, ticks} of positionTicks) {
-			myTicks[line] = (myTicks[line] ?? 0) + ticks;
-		}
-	}
-};
 
 /* NOT FOR BROWSER ONLY END */
 
 const content = readFileSync('test/page.wiki', 'utf8'),
 	{argv: [,, count, method = '']} = process;
 
-/* NOT FOR BROWSER ONLY */
+(async () => {
+	/* NOT FOR BROWSER ONLY */
 
-const session = new inspector.Session();
-session.connect();
-session.post('Profiler.enable', () => {
-	session.post('Profiler.start', () => {
-		/* NOT FOR BROWSER ONLY END */
+	await profile(
+		async () => {
+			/* NOT FOR BROWSER ONLY END */
 
-		(async () => {
 			for (let i = 0; i < (Number(count) || 10); i++) {
 				const page: SimplePage = {content, ns: 0, pageid: 0, title: `Pass ${i}`};
 				if (
@@ -64,36 +38,10 @@ session.post('Profiler.enable', () => {
 			}
 
 			/* NOT FOR BROWSER ONLY */
+		},
 
-			session.post('Profiler.stop', (_, {profile: {nodes}}) => {
-				const useful = nodes.filter(
-						({callFrame: {url}, hitCount, children}) => url.startsWith('file:///')
-							&& (hitCount || children),
-					),
-					summary: ProfileNode[] = [];
-				for (const {callFrame, hitCount, positionTicks} of useful) {
-					const existing = summary.find(
-							({callFrame: {scriptId, lineNumber, columnNumber}}) => callFrame.scriptId === scriptId
-								&& callFrame.lineNumber === lineNumber && callFrame.columnNumber === columnNumber,
-						),
-						myTicks: Record<number, number> = {};
-					addTicks(myTicks, positionTicks);
-					if (existing) {
-						if (hitCount) {
-							existing.hitCount = (existing.hitCount ?? 0) + hitCount;
-						}
-						addTicks(existing.positionTicks, positionTicks);
-					} else {
-						summary.push({callFrame, hitCount, positionTicks: myTicks});
-					}
-				}
-				writeFileSync('test/prof.json', `${JSON.stringify(useful, null, '\t')}\n`);
-				writeFileSync(
-					'test/prof-summary.json',
-					`${JSON.stringify(summary, null, '\t')}\n`,
-				);
-				session.disconnect();
-			});
-		})();
-	});
-});
+		'test',
+	);
+
+	/* NOT FOR BROWSER ONLY END */
+})();
