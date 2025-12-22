5966d1de0b59a756ad63f303f12f292d44fedc6b
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-12-23 00:11:11 +0800

    fix(ImageParameterToken): invalid links in gallery images

--- src/link/galleryImage.ts
+++ src/link/galleryImage.ts
@@ -40,9 +40,10 @@ export abstract class GalleryImageToken extends FileToken {
 			}
 			accum.splice(length, 1);
 		}
-		super(link, token?.firstChild!.toString(), config, accum);
+		const privateType: `${GalleryTypes}-image` = `${type}-image`;
+		super(link, token?.firstChild!.toString(), config, accum, undefined, privateType);
 		this.setAttribute('bracket', false);
-		this.privateType = `${type}-image`;
+		this.privateType = privateType;
 	}
 
 	/** @private */
