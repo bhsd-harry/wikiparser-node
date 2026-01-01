ca840c8f63b4ba3337df194ba4a4182e01cb1016
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-01-01 14:40:03 +0800

    feat(LanguageService): `findJSONTokens()`

--- bump.sh
+++ bump.sh
@@ -2,6 +2,7 @@
 npm run build && npm run lint && npm test && npm run test:real
 if [[ $? -eq 0 ]]
 then
+	gsed -i -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
 	gsed -i -E "s|const version = '.+',|const version = '$1',|" extensions/base.ts
 	npm run build:ext
 	if [[ $? -eq 0 ]]
