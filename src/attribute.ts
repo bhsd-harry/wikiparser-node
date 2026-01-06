e96b6a6d619619929e5b437d7bac720336911d48
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-01-06 20:50:34 +0800

    style: one-line label

--- src/attribute.ts
+++ src/attribute.ts
@@ -338,11 +338,9 @@ export abstract class AttributeToken extends Token {
 
 	/** @private */
 	override escape(): void {
-		LSP: {
-			if (this.type !== 'ext-attr') {
-				this.#equal = '{{=}}';
-				this.lastChild.escape();
-			}
+		LSP: if (this.type !== 'ext-attr') {
+			this.#equal = '{{=}}';
+			this.lastChild.escape();
 		}
 	}
 }
