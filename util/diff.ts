70ec418d381d05150def0489ac608c7cf15fd2ec
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-03-18 20:21:24 +0800

    refactor: import colorization from @bhsd/nodejs

--- util/diff.ts
+++ util/diff.ts
@@ -1,19 +1,17 @@
-import util from 'util';
+import {green, red} from '@bhsd/nodejs';
 
 export type log = (msg: string, ...args: unknown[]) => void;
 
 /* c8 ignore start */
 /** @implements */
 export const error: log = (msg, ...args) => {
-	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-	console.error(util.styleText?.('red', msg) ?? msg, ...args);
+	console.error(red(msg), ...args);
 };
 /* c8 ignore stop */
 
 /* c8 ignore start */
 /** @implements */
 export const info: log = (msg, ...args) => {
-	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-	console.info(util.styleText?.('green', msg) ?? msg, ...args);
+	console.info(green(msg), ...args);
 };
 /* c8 ignore stop */
