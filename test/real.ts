8316f3b4ed4c60dac7f371cd8d5633dd66e26604
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-04-21 15:46:36 +0800

    chore: adjust console outputs

--- test/real.ts
+++ test/real.ts
@@ -54,7 +54,8 @@ const [,, site = ''] = process.argv,
 						await lsp(page, true);
 					}
 				} catch (e) {
-					error(`解析 ${title} 页面时出错！`, e);
+					error(`解析 ${title} 页面时出错！`);
+					console.error(e);
 					failed++;
 				}
 				console.log();
@@ -63,7 +64,8 @@ const [,, site = ''] = process.argv,
 				failures.set(name, failed);
 			}
 		} catch (e) {
-			error(`访问${name}的API端口时出错！`, e);
+			error(`访问${name}的API端口时出错！`);
+			console.error(e);
 		}
 	}
 	if (failures.size > 0) {
