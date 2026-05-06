ea39d0749169ef942588beeb0cb6c42305445014
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-05-06 21:47:09 +0800

    chore: git diff flag

--- diff.sh
+++ diff.sh
@@ -1,9 +1,9 @@
 #!/usr/local/bin/bash
 if (( $# > 2 ))
 then
-	git diff --ignore-all-space --color-moved --minimal "$@"
+	git diff -w --color-moved --minimal "$@"
 else
-	git diff --ignore-all-space --color-moved --minimal --diff-filter=ad "$@" -- *.ts \
+	git diff -w --color-moved --minimal --diff-filter=ad "$@" -- *.ts \
 	bin/ \
 	config/ data/ i18n/ lib/ mixin/ parser/ src/ test/ typings/ util/ \
 	| diff2html -i stdin -F diff.html
