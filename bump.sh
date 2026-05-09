fcf564354a62fc7055887ce5c54e8658ee6b7acb
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-05-09 12:35:51 +0800

    fix(bump.sh): exit if trying to publish

--- bump.sh
+++ bump.sh
@@ -1,16 +1,22 @@
 #!/usr/local/bin/bash
-npm run build && npm run lint && npm test && npm run test:real
-if [[ $? -eq 0 ]]
+if [[ $2 == 'npm' ]]
 then
-	gsed -i -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
-	gsed -i -E "s|const version = '.+',|const version = '$1',|" extensions/base.ts
-	npm run build:ext
+	echo 'npm publish should be run in the main branch!'
+	exit 1
+else
+	npm run build && npm run lint && npm test && npm run test:real
 	if [[ $? -eq 0 ]]
 	then
-		git add -A
-		git commit -m "chore: bump version to v$1-b"
-		git push
-		git tag "v$1-b"
-		git push origin "v$1-b"
+		gsed -i -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
+		gsed -i -E "s|const version = '.+',|const version = '$1',|" extensions/base.ts
+		npm run build:ext
+		if [[ $? -eq 0 ]]
+		then
+			git add -A
+			git commit -m "chore: bump version to v$1-b"
+			git push
+			git tag "v$1-b"
+			git push origin "v$1-b"
+		fi
 	fi
 fi
