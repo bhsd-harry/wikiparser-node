f987cc52a70017ff42d87d92709c61a28324c64e
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-05-09 13:47:53 +0800

    feat(base): `server` property

--- eslint.es8.mjs
+++ eslint.es8.mjs
@@ -1,14 +1,1 @@
-import {dist} from '@bhsd/code-standard';
-import esX from 'eslint-plugin-es-x';
-
-export default [
-	dist,
-	{
-		rules: {
-			...esX.configs['flat/restrict-to-es2017'].rules,
-			'es-x/no-iterator-prototype-filter': 0,
-			'es-x/no-iterator-prototype-map': 0,
-			'es-x/no-iterator-prototype-some': 0,
-		},
-	},
-];
+export {distES8 as default} from '@bhsd/code-standard';
