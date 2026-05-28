531715c7ae577316ec754175a442b954ceb3d198
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-05-28 09:45:20 +0800

    fix: wrong method overloads

--- mixin/elementLike.ts
+++ mixin/elementLike.ts
@@ -6,7 +6,7 @@ import type {
 	AstNodes,
 	Token,
 } from '../internal';
-import type {TokenTypeMap} from '../map';
+import type {TokenTypeMap, SelectedTokenTypes} from '../map';
 
 declare type ElementConstructor = abstract new (...args: any[]) => {
 	readonly childNodes: readonly AstNodes[];
@@ -20,7 +20,7 @@ export interface ElementLike {
 	 * 符合选择器的第一个后代节点
 	 * @param selector selector / 选择器
 	 */
-	querySelector<K extends keyof TokenTypeMap>(selector: K): TokenTypeMap[K] | undefined;
+	querySelector<K extends SelectedTokenTypes>(selector: K): TokenTypeMap[K] | undefined;
 	querySelector<T = Token>(selector: string): T | undefined;
 
 	/**
@@ -29,7 +29,7 @@ export interface ElementLike {
 	 * 符合选择器的所有后代节点
 	 * @param selector selector / 选择器
 	 */
-	querySelectorAll<K extends keyof TokenTypeMap>(selector: K): TokenTypeMap[K][];
+	querySelectorAll<K extends SelectedTokenTypes>(selector: K): TokenTypeMap[K][];
 	querySelectorAll<T = Token>(selector: string): T[];
 
 	/**
@@ -69,8 +69,8 @@ export const elementLike = <S extends ElementConstructor>(constructor: S): S => 
 				return undefined;
 			}
 
-			querySelector<T = Token>(selector: string): T | undefined {
-				return this.getElementBy(this.#getCondition<T>(selector));
+			querySelector(selector: string): Token | undefined {
+				return this.getElementBy(this.#getCondition(selector));
 			}
 
 			getElementsBy<T>(condition: TokenPredicate<T>): T[] {
@@ -91,8 +91,8 @@ export const elementLike = <S extends ElementConstructor>(constructor: S): S => 
 				return descendants;
 			}
 
-			querySelectorAll<T = Token>(selector: string): T[] {
-				return this.getElementsBy(this.#getCondition<T>(selector));
+			querySelectorAll(selector: string): Token[] {
+				return this.getElementsBy(this.#getCondition(selector));
 			}
 
 			escape(): void {
