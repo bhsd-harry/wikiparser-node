294724cc5126db2b99d6a58f8d3a98db99ff3324
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-06-09 12:17:15 +0800

    fix(attributesParent): expose `hasAttr()`

--- mixin/attributesParent.ts
+++ mixin/attributesParent.ts
@@ -33,7 +33,7 @@ export const attributesParent = (i = 0) => <T extends AstConstructor>(constructo
 			}
 
 			hasAttr(key: string): boolean {
-				LSP: return this.#getAttributesChild().hasAttr(key);
+				return this.#getAttributesChild().hasAttr(key);
 			}
 
 			getAttr(key: string): string | undefined {
