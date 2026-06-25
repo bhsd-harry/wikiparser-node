6e325214a1c4ea4017807720b4cfd99dff9ace1a
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-06-25 12:01:29 +0800

    refactor(HiddenToken): `lint()`

--- src/hidden.ts
+++ src/hidden.ts
@@ -2,11 +2,11 @@ import {hiddenToken} from '../mixin/hidden';
 import {Token} from './index';
 
 /**
- * invisible token
+ * invisible token in triple braces
  *
- * 不可见的节点
+ * 三重括号内不可见的节点
  */
-@hiddenToken()
+@hiddenToken(false)
 export class HiddenToken extends Token {
 	override get type(): 'hidden' {
 		return 'hidden';
