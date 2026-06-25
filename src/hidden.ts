9340b159279d5e50123d5640231ab7bec437ec9c
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-06-25 12:01:29 +0800

    refactor(HiddenToken): `lint()`

--- src/hidden.ts
+++ src/hidden.ts
@@ -1,24 +1,54 @@
+import {generateForSelf, fixByRemove, fixByEscape} from '../util/lint';
 import {hiddenToken} from '../mixin/hidden';
 import {noEscape} from '../mixin/noEscape';
+import Parser from '../index';
 import {Token} from './index';
+import type {
+	LintError,
+
+	/* NOT FOR BROWSER */
+
+	Config,
+} from '../base';
 
 /* NOT FOR BROWSER */
 
 import {classes} from '../util/constants';
 import {clone} from '../mixin/clone';
-import type {Config} from '../base';
 
 /* NOT FOR BROWSER END */
 
 /**
- * invisible token
+ * invisible token in triple braces
  *
- * 不可见的节点
+ * 三重括号内不可见的节点
  */
-@hiddenToken() @noEscape
+@hiddenToken(false) @noEscape
 export class HiddenToken extends Token {
 	override get type(): 'hidden' {
 		return 'hidden';
+	}
+
+	/** @private */
+	override lint(start = this.getAbsoluteIndex()): LintError[] {
+		LINT: {
+			const rule = 'no-ignored',
+				{lintConfig} = Parser,
+				s = lintConfig.getSeverity(rule, 'arg');
+			if (s) {
+				const e = generateForSelf(this, {start}, rule, 'invisible-triple-braces', s);
+				e.startIndex--;
+				e.startCol--;
+				if (lintConfig.computeEditInfo) {
+					e.suggestions = [
+						fixByRemove(e),
+						fixByEscape(e.startIndex, '{{!}}'),
+					];
+				}
+				return [e];
+			}
+			return [];
+		}
 	}
 
 	/* PRINT ONLY */
