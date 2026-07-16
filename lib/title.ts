3b68d0fa4c73d2666d1def6a4f65bf82719fe3f9
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-07-16 13:45:16 +0800

    fix: ESLint

--- lib/title.ts
+++ lib/title.ts
@@ -244,8 +244,7 @@ export class Title {
 			if (title) {
 				return this.#path.replace(
 					'$1',
-					encodeURIComponent(title) // eslint-disable-line unicorn/no-unsafe-string-replacement
-					+ (
+					() => encodeURIComponent(title) + (
 						fragment
 							? `#${encodeURIComponent(
 								fragment
--- src/imageParameter.ts
+++ src/imageParameter.ts
@@ -236,8 +236,8 @@ export abstract class ImageParameterToken extends Token {
 
 	/** @private */
 	override toString(skip?: boolean): string {
-		// eslint-disable-next-line unicorn/no-unsafe-string-replacement
-		return this.#syntax ? this.#syntax.replace('$1', super.toString(skip)) : super.toString(skip);
+		const str = super.toString(skip);
+		return this.#syntax ? this.#syntax.replace('$1', () => str) : str;
 	}
 
 	/** @private */
@@ -250,8 +250,7 @@ export abstract class ImageParameterToken extends Token {
 		if (name !== 'caption') {
 			str = str.trim();
 		}
-		// eslint-disable-next-line unicorn/no-unsafe-string-replacement
-		return this.#syntax ? this.#syntax.replace('$1', str).trim() : str;
+		return this.#syntax ? this.#syntax.replace('$1', () => str).trim() : str;
 	}
 
 	/** @private */
@@ -389,8 +388,7 @@ export abstract class ImageParameterToken extends Token {
 				return `<span class="wpb-image-parameter${this.name === 'invalid' ? ' wpb-invalid' : ''}">${
 					this.#syntax.replace(
 						'$1',
-						// eslint-disable-next-line unicorn/no-unsafe-string-replacement
-						`<span class="wpb-image-caption">${print(this.childNodes)}</span>`,
+						() => `<span class="wpb-image-caption">${print(this.childNodes)}</span>`,
 					)
 				}</span>`;
 			}
