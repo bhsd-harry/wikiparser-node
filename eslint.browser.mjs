9f03513dbd8ac2fdd66ae30fecf52c74d43d0069
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-07-25 22:59:06 +0800

    feat(eslint.browser.mjs): support ES10

--- eslint.browser.mjs
+++ eslint.browser.mjs
@@ -1,1 +1,17 @@
-export {dist as default} from '@bhsd/code-standard';
+import {dist, distES10} from '@bhsd/code-standard';
+
+export default [
+	dist,
+	{
+		files: [
+			'bundle/bundle-es*.min.js',
+		],
+		languageOptions: {
+			ecmaVersion: 10,
+		},
+		rules: {
+			...distES10.rules,
+			'es-x/no-global-this': 0,
+		},
+	},
+];
